4a979ba94d5ed726ded0b60c9b3a8bbef8697317
Yeoncomi <yeoncomi@gmail.com>
2017-05-07 21:17:03 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10865,
-	"created_at": "Sun May 07 11:17:03 +0000 2017",
+	"tweets": 10866,
+	"created_at": "Sun May 07 12:17:02 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweets/2017_05.js
+++ data/js/tweets/2017_05.js
@@ -1,6 +1,439 @@
 Grailbird.data.tweets_2017_05 = 
 [
 	{
+		"created_at": "Sun May 07 11:29:47 +0000 2017",
+		"id": 861181263331196900,
+		"id_str": "861181263331196928",
+		"text": "RT @hara_nui: 자~여러분~어느것이 더 게으른 디자인일까요? https://t.co/Q4mZ8ThZ4L",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hara_nui",
+					"name": "하라누이",
+					"id": 737317601462747100,
+					"id_str": "737317601462747140",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 861179431015993300,
+					"id_str": "861179431015993344",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/media/C_OGITmU0AANOnA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_OGITmU0AANOnA.jpg",
+					"url": "https://t.co/Q4mZ8ThZ4L",
+					"display_url": "pic.twitter.com/Q4mZ8ThZ4L",
+					"expanded_url": "https://twitter.com/hara_nui/status/861179448644718592/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 364,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 643,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1097,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 861179448644718600,
+					"source_status_id_str": "861179448644718592",
+					"source_user_id": 737317601462747100,
+					"source_user_id_str": "737317601462747140"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 861179431015993300,
+					"id_str": "861179431015993344",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/media/C_OGITmU0AANOnA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_OGITmU0AANOnA.jpg",
+					"url": "https://t.co/Q4mZ8ThZ4L",
+					"display_url": "pic.twitter.com/Q4mZ8ThZ4L",
+					"expanded_url": "https://twitter.com/hara_nui/status/861179448644718592/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 364,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 643,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1097,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 861179448644718600,
+					"source_status_id_str": "861179448644718592",
+					"source_user_id": 737317601462747100,
+					"source_user_id_str": "737317601462747140"
+				},
+				{
+					"id": 861179444827832300,
+					"id_str": "861179444827832320",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/media/C_OGJHDUwAA5FCv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/C_OGJHDUwAA5FCv.jpg",
+					"url": "https://t.co/Q4mZ8ThZ4L",
+					"display_url": "pic.twitter.com/Q4mZ8ThZ4L",
+					"expanded_url": "https://twitter.com/hara_nui/status/861179448644718592/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 550,
+							"h": 409,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 550,
+							"h": 409,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 550,
+							"h": 409,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 861179448644718600,
+					"source_status_id_str": "861179448644718592",
+					"source_user_id": 737317601462747100,
+					"source_user_id_str": "737317601462747140"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 200682540,
+			"id_str": "200682540",
+			"name": "[5/9일 선거합시다]연컴🎗",
+			"screen_name": "yeoncomi",
+			"location": "Team Lapin",
+			"description": "우울/😷🤒😷 I am a teapot.\n급한 연락은 yeoncomi@gmail.com으로\n시골집에 와있어서 접속을 잘 못합니다\n뒷계: @yeoncomi_NSFW\nParan을 일으키자",
+			"url": "https://t.co/x8nWq5x52R",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x8nWq5x52R",
+							"expanded_url": "https://yeoncomi-nas2.duckdns.org/wordpress",
+							"display_url": "yeoncomi-nas2.duckdns.org/wordpress",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 100,
+			"friends_count": 476,
+			"listed_count": 4,
+			"created_at": "Sat Oct 09 23:35:10 +0000 2010",
+			"favourites_count": 2949,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10819,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/853632614090940416/J-hKjf5u_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/853632614090940416/J-hKjf5u_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/200682540/1490024669",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Sun May 07 11:22:35 +0000 2017",
+			"id": 861179448644718600,
+			"id_str": "861179448644718592",
+			"text": "자~여러분~어느것이 더 게으른 디자인일까요? https://t.co/Q4mZ8ThZ4L",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 861179431015993300,
+						"id_str": "861179431015993344",
+						"indices": [
+							25,
+							48
+						],
+						"media_url": "http://pbs.twimg.com/media/C_OGITmU0AANOnA.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_OGITmU0AANOnA.jpg",
+						"url": "https://t.co/Q4mZ8ThZ4L",
+						"display_url": "pic.twitter.com/Q4mZ8ThZ4L",
+						"expanded_url": "https://twitter.com/hara_nui/status/861179448644718592/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 364,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 643,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1097,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 861179431015993300,
+						"id_str": "861179431015993344",
+						"indices": [
+							25,
+							48
+						],
+						"media_url": "http://pbs.twimg.com/media/C_OGITmU0AANOnA.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_OGITmU0AANOnA.jpg",
+						"url": "https://t.co/Q4mZ8ThZ4L",
+						"display_url": "pic.twitter.com/Q4mZ8ThZ4L",
+						"expanded_url": "https://twitter.com/hara_nui/status/861179448644718592/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 364,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 643,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1097,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 861179444827832300,
+						"id_str": "861179444827832320",
+						"indices": [
+							25,
+							48
+						],
+						"media_url": "http://pbs.twimg.com/media/C_OGJHDUwAA5FCv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/C_OGJHDUwAA5FCv.jpg",
+						"url": "https://t.co/Q4mZ8ThZ4L",
+						"display_url": "pic.twitter.com/Q4mZ8ThZ4L",
+						"expanded_url": "https://twitter.com/hara_nui/status/861179448644718592/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 550,
+								"h": 409,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 550,
+								"h": 409,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 550,
+								"h": 409,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 737317601462747100,
+				"id_str": "737317601462747140",
+				"name": "하라누이",
+				"screen_name": "hara_nui",
+				"location": "",
+				"description": "후원은 문상으로만 받아요",
+				"url": "https://t.co/JseivQlG2D",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/JseivQlG2D",
+								"expanded_url": "http://twpf.jp/hara_nui",
+								"display_url": "twpf.jp/hara_nui",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 547,
+				"friends_count": 359,
+				"listed_count": 11,
+				"created_at": "Mon May 30 16:19:49 +0000 2016",
+				"favourites_count": 131,
+				"utc_offset": 32400,
+				"time_zone": "Seoul",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 98619,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/858697634457862144/rWD4yEZR_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/858697634457862144/rWD4yEZR_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/737317601462747140/1494062492",
+				"profile_link_color": "330099",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 160,
+			"favorite_count": 22,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 160,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Sun May 07 11:11:10 +0000 2017",
 		"id": 861176578759249900,
 		"id_str": "861176578759249920",
