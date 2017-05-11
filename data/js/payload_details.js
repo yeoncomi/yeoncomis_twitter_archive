9cfcb0dbcca4f31a32a343f50ca708348a34b32a
Yeoncomi <yeoncomi@gmail.com>
2017-05-12 01:17:15 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11188,
-	"created_at": "Thu May 11 15:17:09 +0000 2017",
+	"tweets": 11190,
+	"created_at": "Thu May 11 16:17:14 +0000 2017",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2017_05.js",
 		"year": 2017,
 		"var_name": "tweets_2017_05",
-		"tweet_count": 978,
+		"tweet_count": 980,
 		"month": 5
 	},
 	{
