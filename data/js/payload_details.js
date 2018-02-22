98d34062b9d5f19e85aec20267f09d9383f3e59a
Yeoncomi <yeoncomi@gmail.com>
2018-02-22 11:17:18 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 12223,
-	"created_at": "Thu Feb 22 18:17:07 +0000 2018",
+	"tweets": 12224,
+	"created_at": "Thu Feb 22 19:17:17 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_02.js
+++ data/js/tweets/2018_02.js
@@ -1,6 +1,363 @@
 Grailbird.data.tweets_2018_02 = 
 [
 	{
+		"created_at": "Thu Feb 22 19:12:33 +0000 2018",
+		"id": 966752588174581800,
+		"id_str": "966752588174581760",
+		"text": "RT @KI_KYU_: 세상에서 가장 불편한 올림픽 경기 https://t.co/N9jIE3GsOz",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KI_KYU_",
+					"name": "키큐💞/3.29~31 東京",
+					"id": 257271112,
+					"id_str": "257271112",
+					"indices": [
+						3,
+						11
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 965927771703672800,
+					"id_str": "965927771703672832",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/DWeqMM8VAAAj_O6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DWeqMM8VAAAj_O6.jpg",
+					"url": "https://t.co/N9jIE3GsOz",
+					"display_url": "pic.twitter.com/N9jIE3GsOz",
+					"expanded_url": "https://twitter.com/KI_KYU_/status/965927785578479617/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1136,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1136,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 965927785578479600,
+					"source_status_id_str": "965927785578479617",
+					"source_user_id": 257271112,
+					"source_user_id_str": "257271112"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 965927771703672800,
+					"id_str": "965927771703672832",
+					"indices": [
+						32,
+						55
+					],
+					"media_url": "http://pbs.twimg.com/media/DWeqMM8VAAAj_O6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DWeqMM8VAAAj_O6.jpg",
+					"url": "https://t.co/N9jIE3GsOz",
+					"display_url": "pic.twitter.com/N9jIE3GsOz",
+					"expanded_url": "https://twitter.com/KI_KYU_/status/965927785578479617/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1136,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1136,
+							"h": 640,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 965927785578479600,
+					"source_status_id_str": "965927785578479617",
+					"source_user_id": 257271112,
+					"source_user_id_str": "257271112"
+				}
+			]
+		},
+		"source": "<a href=\"http://www.twitter.com\" rel=\"nofollow\">Twitter for Windows</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 200682540,
+			"id_str": "200682540",
+			"name": "💕연컴@캐나다🎗🌈",
+			"screen_name": "yeoncomi",
+			"location": "Maple Ridge, British Columbia",
+			"description": "I am a teapot.|Coldplay좋아합니다 뒷계: @yeoncomi_NSFW|캐나다 유학생|급하면 yeoncomi@gmail.com|개발도 합니다|유학중이라서 트위터 잘 못합니다.",
+			"url": "https://t.co/9GnQQtk86E",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9GnQQtk86E",
+							"expanded_url": "https://yeonco.me",
+							"display_url": "yeonco.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 193,
+			"friends_count": 694,
+			"listed_count": 5,
+			"created_at": "Sat Oct 09 23:35:10 +0000 2010",
+			"favourites_count": 10081,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21391,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/927154637471342593/YTVoBSJC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/927154637471342593/YTVoBSJC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/200682540/1499339251",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Tue Feb 20 12:35:04 +0000 2018",
+			"id": 965927785578479600,
+			"id_str": "965927785578479617",
+			"text": "세상에서 가장 불편한 올림픽 경기 https://t.co/N9jIE3GsOz",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 965927771703672800,
+						"id_str": "965927771703672832",
+						"indices": [
+							19,
+							42
+						],
+						"media_url": "http://pbs.twimg.com/media/DWeqMM8VAAAj_O6.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DWeqMM8VAAAj_O6.jpg",
+						"url": "https://t.co/N9jIE3GsOz",
+						"display_url": "pic.twitter.com/N9jIE3GsOz",
+						"expanded_url": "https://twitter.com/KI_KYU_/status/965927785578479617/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1136,
+								"h": 640,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1136,
+								"h": 640,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 965927771703672800,
+						"id_str": "965927771703672832",
+						"indices": [
+							19,
+							42
+						],
+						"media_url": "http://pbs.twimg.com/media/DWeqMM8VAAAj_O6.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DWeqMM8VAAAj_O6.jpg",
+						"url": "https://t.co/N9jIE3GsOz",
+						"display_url": "pic.twitter.com/N9jIE3GsOz",
+						"expanded_url": "https://twitter.com/KI_KYU_/status/965927785578479617/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1136,
+								"h": 640,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1136,
+								"h": 640,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 257271112,
+				"id_str": "257271112",
+				"name": "키큐💞/3.29~31 東京",
+				"screen_name": "KI_KYU_",
+				"location": "",
+				"description": "レイシス,和泉紗霧💞/ 심한 변태 / 잡다하게 파는중 / DP / 일본어 공부중 / レイシスが大好きな人 /  특정 분들만 아니면 누구나 환영 / 야짤주의, 섹트주의 / 프사는 미루( @05_021 )로 부터! / Team Rasis 팀장 공식계정 @Team_Rasis",
+				"url": "https://t.co/lmCtB5XqmX",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/lmCtB5XqmX",
+								"expanded_url": "http://ask.fm/kikyu774",
+								"display_url": "ask.fm/kikyu774",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 381,
+				"friends_count": 345,
+				"listed_count": 13,
+				"created_at": "Fri Feb 25 02:30:19 +0000 2011",
+				"favourites_count": 10288,
+				"utc_offset": 32400,
+				"time_zone": "Seoul",
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 340481,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/966704818063421441/bw5CAU7u_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/966704818063421441/bw5CAU7u_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/257271112/1512127250",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 18388,
+			"favorite_count": 4114,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 18388,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Thu Feb 22 17:36:42 +0000 2018",
 		"id": 966728468846161900,
 		"id_str": "966728468846161920",
