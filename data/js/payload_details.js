538559acf4b6848eadc8268425842243e3eb1e5c
Yeoncomi <yeoncomi@gmail.com>
2018-03-15 14:17:11 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 12700,
-	"created_at": "Thu Mar 15 15:17:06 +0000 2018",
+	"tweets": 12701,
+	"created_at": "Thu Mar 15 21:17:10 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_03.js
+++ data/js/tweets/2018_03.js
@@ -1,6 +1,350 @@
 Grailbird.data.tweets_2018_03 = 
 [
 	{
+		"created_at": "Thu Mar 15 20:29:57 +0000 2018",
+		"id": 974382211792121900,
+		"id_str": "974382211792121856",
+		"text": "RT @marlred27: 나 코피노가 추정 이만명인지 몰랐어 ........ 이만명이래요... 필리핀에서 아이 임신시키고 한국으로 도망갔다는데 그 아이들이 이만명이래 어떻게 그래... https://t.co/gCShx4XngB",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "marlred27",
+					"name": "웨",
+					"id": 921010230707679200,
+					"id_str": "921010230707679233",
+					"indices": [
+						3,
+						13
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 973995729885675500,
+					"id_str": "973995729885675520",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/media/DYRT9LSVQAAUQUM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DYRT9LSVQAAUQUM.jpg",
+					"url": "https://t.co/gCShx4XngB",
+					"display_url": "pic.twitter.com/gCShx4XngB",
+					"expanded_url": "https://twitter.com/marlred27/status/973995739612262400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 826,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 617,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 826,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 973995739612262400,
+					"source_status_id_str": "973995739612262400",
+					"source_user_id": 921010230707679200,
+					"source_user_id_str": "921010230707679233"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 973995729885675500,
+					"id_str": "973995729885675520",
+					"indices": [
+						104,
+						127
+					],
+					"media_url": "http://pbs.twimg.com/media/DYRT9LSVQAAUQUM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DYRT9LSVQAAUQUM.jpg",
+					"url": "https://t.co/gCShx4XngB",
+					"display_url": "pic.twitter.com/gCShx4XngB",
+					"expanded_url": "https://twitter.com/marlred27/status/973995739612262400/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 826,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 617,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 826,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 973995739612262400,
+					"source_status_id_str": "973995739612262400",
+					"source_user_id": 921010230707679200,
+					"source_user_id_str": "921010230707679233"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 200682540,
+			"id_str": "200682540",
+			"name": "래디컬 유학생🎗🌈",
+			"screen_name": "yeoncomi",
+			"location": "Maple Ridge, British Columbia",
+			"description": "I am a teapot.|Coldplay좋아합니다 뒷계: @yeoncomi_NSFW|캐나다 유학생|급하면 yeoncomi@gmail.com|개발도 합니다|유학중이라서 트위터 잘 못합니다.",
+			"url": "https://t.co/9GnQQtk86E",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9GnQQtk86E",
+							"expanded_url": "https://yeonco.me",
+							"display_url": "yeonco.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 192,
+			"friends_count": 702,
+			"listed_count": 5,
+			"created_at": "Sat Oct 09 23:35:10 +0000 2010",
+			"favourites_count": 10412,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 21739,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/927154637471342593/YTVoBSJC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/927154637471342593/YTVoBSJC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/200682540/1499339251",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Wed Mar 14 18:54:14 +0000 2018",
+			"id": 973995739612262400,
+			"id_str": "973995739612262400",
+			"text": "나 코피노가 추정 이만명인지 몰랐어 ........ 이만명이래요... 필리핀에서 아이 임신시키고 한국으로 도망갔다는데 그 아이들이 이만명이래 어떻게 그래... https://t.co/gCShx4XngB",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 973995729885675500,
+						"id_str": "973995729885675520",
+						"indices": [
+							89,
+							112
+						],
+						"media_url": "http://pbs.twimg.com/media/DYRT9LSVQAAUQUM.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DYRT9LSVQAAUQUM.jpg",
+						"url": "https://t.co/gCShx4XngB",
+						"display_url": "pic.twitter.com/gCShx4XngB",
+						"expanded_url": "https://twitter.com/marlred27/status/973995739612262400/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 750,
+								"h": 826,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 617,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 826,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 973995729885675500,
+						"id_str": "973995729885675520",
+						"indices": [
+							89,
+							112
+						],
+						"media_url": "http://pbs.twimg.com/media/DYRT9LSVQAAUQUM.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DYRT9LSVQAAUQUM.jpg",
+						"url": "https://t.co/gCShx4XngB",
+						"display_url": "pic.twitter.com/gCShx4XngB",
+						"expanded_url": "https://twitter.com/marlred27/status/973995739612262400/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 750,
+								"h": 826,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 617,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 826,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 921010230707679200,
+				"id_str": "921010230707679233",
+				"name": "웨",
+				"screen_name": "marlred27",
+				"location": "프사 @dermond5447",
+				"description": "종합비타민 챙겨먹고 헛짇음이 줄었어요",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 234,
+				"friends_count": 152,
+				"listed_count": 1,
+				"created_at": "Thu Oct 19 13:48:45 +0000 2017",
+				"favourites_count": 101,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 16647,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/955046896136941569/e9O7NyNG_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/955046896136941569/e9O7NyNG_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/921010230707679233/1518538684",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 21862,
+			"favorite_count": 3104,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 21862,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Thu Mar 15 14:34:25 +0000 2018",
 		"id": 974292741570965500,
 		"id_str": "974292741570965504",
