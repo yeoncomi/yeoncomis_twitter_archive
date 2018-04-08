c168ffb68c5c7946e6f425be09540a2a870ee323
Yeoncomi <yeoncomi@gmail.com>
2018-04-07 21:17:04 -0700

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13278,
-	"created_at": "Sun Apr 08 03:17:02 +0000 2018",
+	"tweets": 13280,
+	"created_at": "Sun Apr 08 04:17:03 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,603 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Sun Apr 08 04:16:44 +0000 2018",
+		"id": 982834603386982400,
+		"id_str": "982834603386982401",
+		"text": "@_Hoto_Cocoa_ yup I use hynix+samsung",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "_Hoto_Cocoa_",
+					"name": "Hoto Cocoa",
+					"id": 3378121573,
+					"id_str": "3378121573",
+					"indices": [
+						0,
+						13
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://www.twitter.com\" rel=\"nofollow\">Twitter for BlackBerry</a>",
+		"in_reply_to_status_id": 982829863898763300,
+		"in_reply_to_status_id_str": "982829863898763264",
+		"in_reply_to_user_id": 3378121573,
+		"in_reply_to_user_id_str": "3378121573",
+		"in_reply_to_screen_name": "_Hoto_Cocoa_",
+		"user": {
+			"id": 200682540,
+			"id_str": "200682540",
+			"name": "래디컬 유학생🎗🌈△",
+			"screen_name": "yeoncomi",
+			"location": "Maple Ridge, British Columbia",
+			"description": "I am a teapot.|Coldplay좋아합니다 뒷계: @yeoncomi_NSFW|캐나다 유학생|급하면 yeoncomi@gmail.com|개발도 합니다|유학중이라서 트위터 잘 못합니다. |#GirlsCanDoAnything",
+			"url": "https://t.co/9GnQQtk86E",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9GnQQtk86E",
+							"expanded_url": "https://yeonco.me",
+							"display_url": "yeonco.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 189,
+			"friends_count": 708,
+			"listed_count": 5,
+			"created_at": "Sat Oct 09 23:35:10 +0000 2010",
+			"favourites_count": 10813,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22141,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/927154637471342593/YTVoBSJC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/927154637471342593/YTVoBSJC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/200682540/1499339251",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en"
+	},
+	{
+		"created_at": "Sun Apr 08 04:12:42 +0000 2018",
+		"id": 982833591313993700,
+		"id_str": "982833591313993728",
+		"text": "RT @studyasfuck: ???:독일은 유명한 세계적 메이커는 없습니다. https://t.co/zYE4VTCDdb",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "studyasfuck",
+					"name": "우칸지",
+					"id": 771464102,
+					"id_str": "771464102",
+					"indices": [
+						3,
+						15
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 982366695766474800,
+					"id_str": "982366695766474757",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/DaIRThRVwAUKFm-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaIRThRVwAUKFm-.jpg",
+					"url": "https://t.co/zYE4VTCDdb",
+					"display_url": "pic.twitter.com/zYE4VTCDdb",
+					"expanded_url": "https://twitter.com/studyasfuck/status/982366703723036673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 898,
+							"h": 502,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 898,
+							"h": 502,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 982366703723036700,
+					"source_status_id_str": "982366703723036673",
+					"source_user_id": 771464102,
+					"source_user_id_str": "771464102"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 982366695766474800,
+					"id_str": "982366695766474757",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/DaIRThRVwAUKFm-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaIRThRVwAUKFm-.jpg",
+					"url": "https://t.co/zYE4VTCDdb",
+					"display_url": "pic.twitter.com/zYE4VTCDdb",
+					"expanded_url": "https://twitter.com/studyasfuck/status/982366703723036673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 380,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 898,
+							"h": 502,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 898,
+							"h": 502,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 982366703723036700,
+					"source_status_id_str": "982366703723036673",
+					"source_user_id": 771464102,
+					"source_user_id_str": "771464102"
+				},
+				{
+					"id": 982366695774810100,
+					"id_str": "982366695774810112",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/DaIRThTU8AAZ_po.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaIRThTU8AAZ_po.jpg",
+					"url": "https://t.co/zYE4VTCDdb",
+					"display_url": "pic.twitter.com/zYE4VTCDdb",
+					"expanded_url": "https://twitter.com/studyasfuck/status/982366703723036673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 608,
+							"h": 344,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 608,
+							"h": 344,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 608,
+							"h": 344,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 982366703723036700,
+					"source_status_id_str": "982366703723036673",
+					"source_user_id": 771464102,
+					"source_user_id_str": "771464102"
+				},
+				{
+					"id": 982366695778996200,
+					"id_str": "982366695778996226",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/DaIRThUU0AI2hL0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaIRThUU0AI2hL0.jpg",
+					"url": "https://t.co/zYE4VTCDdb",
+					"display_url": "pic.twitter.com/zYE4VTCDdb",
+					"expanded_url": "https://twitter.com/studyasfuck/status/982366703723036673/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 307,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 609,
+							"h": 1350,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 541,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 982366703723036700,
+					"source_status_id_str": "982366703723036673",
+					"source_user_id": 771464102,
+					"source_user_id_str": "771464102"
+				}
+			]
+		},
+		"source": "<a href=\"http://www.twitter.com\" rel=\"nofollow\">Twitter for BlackBerry</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 200682540,
+			"id_str": "200682540",
+			"name": "래디컬 유학생🎗🌈△",
+			"screen_name": "yeoncomi",
+			"location": "Maple Ridge, British Columbia",
+			"description": "I am a teapot.|Coldplay좋아합니다 뒷계: @yeoncomi_NSFW|캐나다 유학생|급하면 yeoncomi@gmail.com|개발도 합니다|유학중이라서 트위터 잘 못합니다. |#GirlsCanDoAnything",
+			"url": "https://t.co/9GnQQtk86E",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9GnQQtk86E",
+							"expanded_url": "https://yeonco.me",
+							"display_url": "yeonco.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 189,
+			"friends_count": 708,
+			"listed_count": 5,
+			"created_at": "Sat Oct 09 23:35:10 +0000 2010",
+			"favourites_count": 10813,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 22141,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/927154637471342593/YTVoBSJC_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/927154637471342593/YTVoBSJC_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/200682540/1499339251",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Apr 06 21:17:28 +0000 2018",
+			"id": 982366703723036700,
+			"id_str": "982366703723036673",
+			"text": "???:독일은 유명한 세계적 메이커는 없습니다. https://t.co/zYE4VTCDdb",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 982366695766474800,
+						"id_str": "982366695766474757",
+						"indices": [
+							27,
+							50
+						],
+						"media_url": "http://pbs.twimg.com/media/DaIRThRVwAUKFm-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaIRThRVwAUKFm-.jpg",
+						"url": "https://t.co/zYE4VTCDdb",
+						"display_url": "pic.twitter.com/zYE4VTCDdb",
+						"expanded_url": "https://twitter.com/studyasfuck/status/982366703723036673/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 380,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 898,
+								"h": 502,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 898,
+								"h": 502,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 982366695766474800,
+						"id_str": "982366695766474757",
+						"indices": [
+							27,
+							50
+						],
+						"media_url": "http://pbs.twimg.com/media/DaIRThRVwAUKFm-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaIRThRVwAUKFm-.jpg",
+						"url": "https://t.co/zYE4VTCDdb",
+						"display_url": "pic.twitter.com/zYE4VTCDdb",
+						"expanded_url": "https://twitter.com/studyasfuck/status/982366703723036673/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 380,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 898,
+								"h": 502,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 898,
+								"h": 502,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 982366695774810100,
+						"id_str": "982366695774810112",
+						"indices": [
+							27,
+							50
+						],
+						"media_url": "http://pbs.twimg.com/media/DaIRThTU8AAZ_po.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaIRThTU8AAZ_po.jpg",
+						"url": "https://t.co/zYE4VTCDdb",
+						"display_url": "pic.twitter.com/zYE4VTCDdb",
+						"expanded_url": "https://twitter.com/studyasfuck/status/982366703723036673/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 608,
+								"h": 344,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 608,
+								"h": 344,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 608,
+								"h": 344,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 982366695778996200,
+						"id_str": "982366695778996226",
+						"indices": [
+							27,
+							50
+						],
+						"media_url": "http://pbs.twimg.com/media/DaIRThUU0AI2hL0.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaIRThUU0AI2hL0.jpg",
+						"url": "https://t.co/zYE4VTCDdb",
+						"display_url": "pic.twitter.com/zYE4VTCDdb",
+						"expanded_url": "https://twitter.com/studyasfuck/status/982366703723036673/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 307,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 609,
+								"h": 1350,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 541,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 771464102,
+				"id_str": "771464102",
+				"name": "우칸지",
+				"screen_name": "studyasfuck",
+				"location": "",
+				"description": "오호 적이여, 너는 나의 용기이다",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1611,
+				"friends_count": 99,
+				"listed_count": 10,
+				"created_at": "Tue Aug 21 12:57:30 +0000 2012",
+				"favourites_count": 5619,
+				"utc_offset": -25200,
+				"time_zone": "Pacific Time (US & Canada)",
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1177,
+				"lang": "ko",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/981004550931808256/qk1PBgU0_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/981004550931808256/qk1PBgU0_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/771464102/1522539748",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1698,
+			"favorite_count": 350,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "ko"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1698,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko"
+	},
+	{
 		"created_at": "Sun Apr 08 03:13:11 +0000 2018",
 		"id": 982818609553596400,
 		"id_str": "982818609553596422",
