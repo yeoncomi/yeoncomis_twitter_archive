e40312c8464674618d374c3c51d78592fc863db4
Yeoncomi <yeoncomi@gmail.com>
2018-04-20 21:17:15 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13820,
-	"created_at": "Sat Apr 21 03:17:14 +0000 2018",
+	"tweets": 13821,
+	"created_at": "Sat Apr 21 04:17:14 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_04.js",
 		"year": 2018,
 		"var_name": "tweets_2018_04",
-		"tweet_count": 756,
+		"tweet_count": 757,
 		"month": 4
 	},
 	{
