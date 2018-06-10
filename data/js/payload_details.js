0eab496b833469c875627a2536b72a5f7b9797d4
Yeoncomi <yeoncomi@gmail.com>
2018-06-10 15:17:09 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15958,
-	"created_at": "Sun Jun 10 18:17:08 +0000 2018",
+	"tweets": 15959,
+	"created_at": "Sun Jun 10 22:17:08 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_06.js",
 		"year": 2018,
 		"var_name": "tweets_2018_06",
-		"tweet_count": 530,
+		"tweet_count": 531,
 		"month": 6
 	},
 	{
