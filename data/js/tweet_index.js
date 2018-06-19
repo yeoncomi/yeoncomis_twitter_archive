76b9f4336e8fa8b8ffa1bfa81651ba7ce4874241
Yeoncomi <yeoncomi@gmail.com>
2018-06-19 09:17:24 -0700

    Added 2 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_06.js",
 		"year": 2018,
 		"var_name": "tweets_2018_06",
-		"tweet_count": 886,
+		"tweet_count": 888,
 		"month": 6
 	},
 	{
