97c9b8b2bb2d235da175f6050d020cb95b18cbae
Yeoncomi <yeoncomi@gmail.com>
2018-12-20 03:17:03 -0800

    Added 1 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 502,
+		"tweet_count": 503,
 		"month": 12
 	},
 	{
