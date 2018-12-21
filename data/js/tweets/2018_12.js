9c22e94946fdf8b1e4baba16163011183ca2b452
Yeoncomi <yeoncomi@gmail.com>
2018-12-20 18:17:04 -0800

    Added 3 tweets

--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -131,6 +131,559 @@ Grailbird.data.tweets_2018_12 =
 			"friends_count": 892,
 			"listed_count": 45,
 			"created_at": "Sun Jan 04 02:08:56 +0000 2015",
+			"favourites_count": 4106,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 192644,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1072989043057557505/RXYF9Jrm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1072989043057557505/RXYF9Jrm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2957863214/1537412510",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 489,
+		"favorite_count": 216,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "웨얼이즈 분유코너 https://t.co/Ga5g56q8Kn"
+	},
+	{
+		"created_at": "Thu Dec 20 15:25:36 +0000 2018",
+		"id": 1075774225607143400,
+		"id_str": "1075774225607143424",
+		"full_text": "심영물 집단 차단에 벌써부터 대처하는 심영물 장인들\n\n판화효과에 점묘화라닠ㅋㅋㅋㅋㅋㅋㅌㅋㅌ https://t.co/XNJCi72SLl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			50
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1075774213204598800,
+					"id_str": "1075774213204598784",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/Du3q8nyV4AAN2Db.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du3q8nyV4AAN2Db.jpg",
+					"url": "https://t.co/XNJCi72SLl",
+					"display_url": "pic.twitter.com/XNJCi72SLl",
+					"expanded_url": "https://twitter.com/Croos_z_FoX/status/1075774225607143424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 503,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 533,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 533,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1075774213204598800,
+					"id_str": "1075774213204598784",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/Du3q8nyV4AAN2Db.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du3q8nyV4AAN2Db.jpg",
+					"url": "https://t.co/XNJCi72SLl",
+					"display_url": "pic.twitter.com/XNJCi72SLl",
+					"expanded_url": "https://twitter.com/Croos_z_FoX/status/1075774225607143424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 503,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 533,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 533,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1075774220070576100,
+					"id_str": "1075774220070576132",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/Du3q9BXUYAQg9D8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du3q9BXUYAQg9D8.jpg",
+					"url": "https://t.co/XNJCi72SLl",
+					"display_url": "pic.twitter.com/XNJCi72SLl",
+					"expanded_url": "https://twitter.com/Croos_z_FoX/status/1075774225607143424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 498,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 527,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 527,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 963456261277626400,
+			"id_str": "963456261277626369",
+			"name": "라인@VRC :З",
+			"screen_name": "Croos_z_FoX",
+			"location": "VRC_IS_MY_LIFE_UNDERSTAND?",
+			"description": "브알쳇 시작했다데스",
+			"url": "https://t.co/wFkLjNRwmF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wFkLjNRwmF",
+							"expanded_url": "http://xn--3h3bmlu1w.com",
+							"display_url": "백병원.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 199,
+			"friends_count": 196,
+			"listed_count": 13,
+			"created_at": "Tue Feb 13 16:54:07 +0000 2018",
+			"favourites_count": 6886,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 27903,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1074009581628448768/JgFgp4_9_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1074009581628448768/JgFgp4_9_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/963456261277626369/1545209200",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 169,
+		"favorite_count": 65,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "심영물 집단 차단에 벌써부터 대처하는 심영물 장인들\n\n판화효과에 점묘화라닠ㅋㅋㅋㅋㅋㅋㅌㅋㅌ https://t.co/XNJCi72SLl"
+	},
+	{
+		"created_at": "Thu Dec 20 06:34:12 +0000 2018",
+		"id": 1075640493948555300,
+		"id_str": "1075640493948555265",
+		"full_text": "#나랑_친해지고_싶다_rt_나랑_반모하고_싶다_마음_짱친확인_멘션_사귀자_디엠\n\n교류를 \n시작하지 \n네덕새끼들죽이고십다 https://t.co/6JlFcDxV1z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			66
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "나랑_친해지고_싶다_rt_나랑_반모하고_싶다_마음_짱친확인_멘션_사귀자_디엠",
+					"indices": [
+						0,
+						43
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1075640398205280300,
+					"id_str": "1075640398205280257",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/Du1xPkEV4AEX5UC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du1xPkEV4AEX5UC.jpg",
+					"url": "https://t.co/6JlFcDxV1z",
+					"display_url": "pic.twitter.com/6JlFcDxV1z",
+					"expanded_url": "https://twitter.com/SATAN_KOHAI/status/1075640493948555265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1075640398205280300,
+					"id_str": "1075640398205280257",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/Du1xPkEV4AEX5UC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du1xPkEV4AEX5UC.jpg",
+					"url": "https://t.co/6JlFcDxV1z",
+					"display_url": "pic.twitter.com/6JlFcDxV1z",
+					"expanded_url": "https://twitter.com/SATAN_KOHAI/status/1075640493948555265/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 935120525222744000,
+			"id_str": "935120525222744064",
+			"name": "yanderen.🌙",
+			"screen_name": "SATAN_KOHAI",
+			"location": "@zed__of__ren",
+			"description": "얀데레도 얼굴 가려가면서 집착해 일단 넌 아냐 걱정마.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 697,
+			"friends_count": 541,
+			"listed_count": 59,
+			"created_at": "Mon Nov 27 12:18:01 +0000 2017",
+			"favourites_count": 16594,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 41123,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1072036915333881857/ApyPIVEy_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1072036915333881857/ApyPIVEy_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/935120525222744064/1541308993",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 68,
+		"favorite_count": 25,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#나랑_친해지고_싶다_rt_나랑_반모하고_싶다_마음_짱친확인_멘션_사귀자_디엠\n\n교류를 \n시작하지 \n네덕새끼들죽이고십다 https://t.co/6JlFcDxV1z"
+	},
+	{
+		"created_at": "Thu Dec 20 05:06:02 +0000 2018",
+		"id": 1075618303882670100,
+		"id_str": "1075618303882670081",
+		"full_text": "웨얼이즈 분유코너 https://t.co/Ga5g56q8Kn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1075618293283676200,
+					"id_str": "1075618293283676160",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/media/Du1dI40V4AA2xMy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du1dI40V4AA2xMy.jpg",
+					"url": "https://t.co/Ga5g56q8Kn",
+					"display_url": "pic.twitter.com/Ga5g56q8Kn",
+					"expanded_url": "https://twitter.com/RenfuuP/status/1075618303882670081/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 700,
+							"h": 933,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 933,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1075618293283676200,
+					"id_str": "1075618293283676160",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/media/Du1dI40V4AA2xMy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du1dI40V4AA2xMy.jpg",
+					"url": "https://t.co/Ga5g56q8Kn",
+					"display_url": "pic.twitter.com/Ga5g56q8Kn",
+					"expanded_url": "https://twitter.com/RenfuuP/status/1075618303882670081/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 700,
+							"h": 933,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 700,
+							"h": 933,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2957863214,
+			"id_str": "2957863214",
+			"name": "렌푸",
+			"screen_name": "RenfuuP",
+			"location": "Grifon & Ktryuger",
+			"description": "assistant manager /  PSN: RenfuuP00 / 그림 그릴줄 모릅니다 / 우마무스메 좋아해요 /체인블락 돌렸습니다. / 언팔은 오류",
+			"url": "https://t.co/oj0nWS350i",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/oj0nWS350i",
+							"expanded_url": "https://www.pixiv.net/member.php?id=758366",
+							"display_url": "pixiv.net/member.php?id=…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3504,
+			"friends_count": 892,
+			"listed_count": 45,
+			"created_at": "Sun Jan 04 02:08:56 +0000 2015",
 			"favourites_count": 4105,
 			"utc_offset": null,
 			"time_zone": null,
