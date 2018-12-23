67c3f9a57df9a3219ef42dffa6b4574c9b6b399d
Yeoncomi <yeoncomi@gmail.com>
2018-12-23 01:17:06 -0800

    Added 25 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 22495,
-	"created_at": "Sun Dec 23 08:17:17 +0000 2018",
+	"tweets": 22520,
+	"created_at": "Sun Dec 23 09:17:05 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 826,
+		"tweet_count": 851,
 		"month": 12
 	},
 	{
