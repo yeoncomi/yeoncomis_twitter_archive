aecce7abd20bc5fa966ce1b1b8fb3ca9387afa23
Yeoncomi <yeoncomi@gmail.com>
2019-01-12 12:17:05 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28922,
-	"created_at": "Sat Jan 12 19:17:05 +0000 2019",
+	"tweets": 28923,
+	"created_at": "Sat Jan 12 20:17:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sat Jan 12 18:20:52 +0000 2019",
+		"id": 1084153253598806000,
+		"id_str": "1084168361531985920",
+		"full_text": "I know the answer to this one https://t.co/1XS7Htq9Kl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1084153249370947600,
+					"id_str": "1084153249370947585",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/Dwuvot7XQAEHRzs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dwuvot7XQAEHRzs.jpg",
+					"url": "https://t.co/1XS7Htq9Kl",
+					"display_url": "pic.twitter.com/1XS7Htq9Kl",
+					"expanded_url": "https://twitter.com/tomwarren/status/1084153253598806018/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 574,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1332,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1014,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1084153249370947600,
+					"id_str": "1084153249370947585",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/Dwuvot7XQAEHRzs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dwuvot7XQAEHRzs.jpg",
+					"url": "https://t.co/1XS7Htq9Kl",
+					"display_url": "pic.twitter.com/1XS7Htq9Kl",
+					"expanded_url": "https://twitter.com/tomwarren/status/1084153253598806018/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 574,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1332,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1014,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 12273252,
+			"id_str": "12273252",
+			"name": "Tom Warren",
+			"screen_name": "tomwarren",
+			"location": "London, England",
+			"description": "Senior Editor at The Verge. My Twitter DMs are open or you can email me (tom@theverge.com)",
+			"url": "https://t.co/DEJ5KzBs4Q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DEJ5KzBs4Q",
+							"expanded_url": "https://www.theverge.com/users/tomwarren/posts",
+							"display_url": "theverge.com/users/tomwarre…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 77284,
+			"friends_count": 1425,
+			"listed_count": 2461,
+			"created_at": "Tue Jan 15 14:42:09 +0000 2008",
+			"favourites_count": 3744,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 73453,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1007122170579963904/1hBOxw5H_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1007122170579963904/1hBOxw5H_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/12273252/1533745085",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 56,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I know the answer to this one https://t.co/1XS7Htq9Kl"
+	},
+	{
 		"created_at": "Sat Jan 12 18:53:43 +0000 2019",
 		"id": 1084161517824680000,
 		"id_str": "1084165172145475584",
