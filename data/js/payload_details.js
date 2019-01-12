acbfc782b89f023892a45e8f2a27850669f0e400
Yeoncomi <yeoncomi@gmail.com>
2019-01-12 11:17:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28921,
-	"created_at": "Sat Jan 12 11:17:04 +0000 2019",
+	"tweets": 28922,
+	"created_at": "Sat Jan 12 19:17:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sat Jan 12 18:53:43 +0000 2019",
+		"id": 1084161517824680000,
+		"id_str": "1084165172145475584",
+		"full_text": "aws advertisements everywhere https://t.co/EzvAFnaaVI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1084161506122571800,
+					"id_str": "1084161506122571778",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/Dwu3JUuWoAIVIqZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dwu3JUuWoAIVIqZ.jpg",
+					"url": "https://t.co/EzvAFnaaVI",
+					"display_url": "pic.twitter.com/EzvAFnaaVI",
+					"expanded_url": "https://twitter.com/Kintiru/status/1084161517824679941/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1032,
+							"h": 581,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1032,
+							"h": 581,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1084161506122571800,
+					"id_str": "1084161506122571778",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/Dwu3JUuWoAIVIqZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dwu3JUuWoAIVIqZ.jpg",
+					"url": "https://t.co/EzvAFnaaVI",
+					"display_url": "pic.twitter.com/EzvAFnaaVI",
+					"expanded_url": "https://twitter.com/Kintiru/status/1084161517824679941/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1032,
+							"h": 581,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1032,
+							"h": 581,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 855244594954612700,
+			"id_str": "855244594954612736",
+			"name": "❄️ 티루",
+			"screen_name": "Kintiru",
+			"location": "빙하 속",
+			"description": "Playing @Rainbow6Game",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 224,
+			"friends_count": 406,
+			"listed_count": 2,
+			"created_at": "Fri Apr 21 02:19:35 +0000 2017",
+			"favourites_count": 23919,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 44185,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1075712533523181568/iZO3SzjJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1075712533523181568/iZO3SzjJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/855244594954612736/1546354935",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "aws advertisements everywhere https://t.co/EzvAFnaaVI"
+	},
+	{
 		"created_at": "Fri Jan 11 18:08:42 +0000 2019",
 		"id": 1083787801898868700,
 		"id_str": "1084037654264467457",
