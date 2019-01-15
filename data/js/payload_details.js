999f473b0aa61a2dd3f165518b857c8ecdb2349e
Yeoncomi <yeoncomi@gmail.com>
2019-01-15 09:17:09 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28983,
-	"created_at": "Tue Jan 15 13:17:07 +0000 2019",
+	"tweets": 28984,
+	"created_at": "Tue Jan 15 17:17:08 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,239 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 15 09:04:45 +0000 2019",
+		"id": 1085100462506836000,
+		"id_str": "1085217374809149440",
+		"full_text": "트위터의 잘그림의 기준 좀 난해한데 https://t.co/vTX4cr0xh0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			19
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1085100047224647700,
+					"id_str": "1085100047224647681",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw8MvmyVsAE9SCw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw8MvmyVsAE9SCw.jpg",
+					"url": "https://t.co/vTX4cr0xh0",
+					"display_url": "pic.twitter.com/vTX4cr0xh0",
+					"expanded_url": "https://twitter.com/i_love_shotgun/status/1085100462506835968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 578,
+							"h": 626,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 578,
+							"h": 626,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 578,
+							"h": 626,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1085100047224647700,
+					"id_str": "1085100047224647681",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw8MvmyVsAE9SCw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw8MvmyVsAE9SCw.jpg",
+					"url": "https://t.co/vTX4cr0xh0",
+					"display_url": "pic.twitter.com/vTX4cr0xh0",
+					"expanded_url": "https://twitter.com/i_love_shotgun/status/1085100462506835968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 578,
+							"h": 626,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 578,
+							"h": 626,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 578,
+							"h": 626,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1085100219690176500,
+					"id_str": "1085100219690176512",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw8M5pRU0AAZRaV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw8M5pRU0AAZRaV.jpg",
+					"url": "https://t.co/vTX4cr0xh0",
+					"display_url": "pic.twitter.com/vTX4cr0xh0",
+					"expanded_url": "https://twitter.com/i_love_shotgun/status/1085100462506835968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 602,
+							"h": 292,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 602,
+							"h": 292,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 602,
+							"h": 292,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1085100335754993700,
+					"id_str": "1085100335754993665",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw8NAZpVYAE6kA6.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw8NAZpVYAE6kA6.jpg",
+					"url": "https://t.co/vTX4cr0xh0",
+					"display_url": "pic.twitter.com/vTX4cr0xh0",
+					"expanded_url": "https://twitter.com/i_love_shotgun/status/1085100462506835968/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 600,
+							"h": 722,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 600,
+							"h": 722,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 565,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 715749726402977800,
+			"id_str": "715749726402977797",
+			"name": "샷건빌런",
+			"screen_name": "i_love_shotgun",
+			"location": "",
+			"description": "케모노 프렌즈 2기 언제 나오냐",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3558,
+			"friends_count": 479,
+			"listed_count": 64,
+			"created_at": "Fri Apr 01 03:56:47 +0000 2016",
+			"favourites_count": 1625,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 87585,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1077969463708794880/Lo0KHZj__normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1077969463708794880/Lo0KHZj__normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/715749726402977797/1512789734",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 444,
+		"favorite_count": 135,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "트위터의 잘그림의 기준 좀 난해한데 https://t.co/vTX4cr0xh0"
+	},
+	{
 		"created_at": "Tue Jan 15 13:02:21 +0000 2019",
 		"id": 1085160258660515800,
 		"id_str": "1085160258660515841",
