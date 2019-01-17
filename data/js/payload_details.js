19cbf05219fafc6e76444972db011d65579abb43
Yeoncomi <yeoncomi@gmail.com>
2019-01-16 20:17:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 28999,
-	"created_at": "Thu Jan 17 03:17:05 +0000 2019",
+	"tweets": 29000,
+	"created_at": "Thu Jan 17 04:17:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Tue Jan 15 08:25:30 +0000 2019",
+		"id": 1085090585046741000,
+		"id_str": "1085741555493699584",
+		"full_text": "경상도말에서의 노는 육하원칙에 따른 의문문에만 붙습니다. '난 일베가 아니라 사투리 썼다' 변명하면서 아름다운 경상도말 이상하게 만들지나 마세요.\n\n예) 니 뭐하노? ○\n      이거 재밌노 ×",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			109
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 963776183165894700,
+			"id_str": "963776183165894656",
+			"name": "Servus Dei❤TAEBO❤",
+			"screen_name": "sal_fermentum",
+			"location": "누룩이 익어가고 있습니다",
+			"description": "03/☧/Francisco/알아가고 있는 중/",
+			"url": "https://t.co/IHRhDsEl67",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/IHRhDsEl67",
+							"expanded_url": "http://www.taebo.com",
+							"display_url": "taebo.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 166,
+			"friends_count": 241,
+			"listed_count": 1,
+			"created_at": "Wed Feb 14 14:05:22 +0000 2018",
+			"favourites_count": 15886,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 628,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1082569392745897984/CBSgDGbT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1082569392745897984/CBSgDGbT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/963776183165894656/1544941749",
+			"profile_link_color": "FFFFFF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1862,
+		"favorite_count": 392,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "경상도말에서의 노는 육하원칙에 따른 의문문에만 붙습니다. '난 일베가 아니라 사투리 썼다' 변명하면서 아름다운 경상도말 이상하게 만들지나 마세요.\n\n예) 니 뭐하노? ○\n      이거 재밌노 ×"
+	},
+	{
 		"created_at": "Thu Jan 17 02:22:08 +0000 2019",
 		"id": 1085723918550192100,
 		"id_str": "1085723918550192129",
