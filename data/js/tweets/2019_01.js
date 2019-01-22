decba771a713335ea2b3e7b37412b43828b89a5c
Yeoncomi <yeoncomi@gmail.com>
2019-01-22 10:17:15 -0800

    Added 1 tweets

--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Mon Jan 21 19:52:21 +0000 2019",
+		"id": 1087437764486778900,
+		"id_str": "1087768406311370752",
+		"full_text": "오버워치 영웅 솔져:76이 동성애자라는 설정에 대한 나무위키의 서술. 이 땅의 모든 혐오는 장작위키로 모인다. https://t.co/1NIy5sMkHo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			61
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1087437616633471000,
+					"id_str": "1087437616633470976",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/DxdawDbV4AAY5pA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxdawDbV4AAY5pA.jpg",
+					"url": "https://t.co/1NIy5sMkHo",
+					"display_url": "pic.twitter.com/1NIy5sMkHo",
+					"expanded_url": "https://twitter.com/kredu02/status/1087437764486778880/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 538,
+							"h": 203,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 538,
+							"h": 203,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 538,
+							"h": 203,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1087437616633471000,
+					"id_str": "1087437616633470976",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/DxdawDbV4AAY5pA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxdawDbV4AAY5pA.jpg",
+					"url": "https://t.co/1NIy5sMkHo",
+					"display_url": "pic.twitter.com/1NIy5sMkHo",
+					"expanded_url": "https://twitter.com/kredu02/status/1087437764486778880/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 538,
+							"h": 203,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 538,
+							"h": 203,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 538,
+							"h": 203,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1087437626422915100,
+					"id_str": "1087437626422915072",
+					"indices": [
+						62,
+						85
+					],
+					"media_url": "http://pbs.twimg.com/media/Dxdawn5U8AARBVx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dxdawn5U8AARBVx.jpg",
+					"url": "https://t.co/1NIy5sMkHo",
+					"display_url": "pic.twitter.com/1NIy5sMkHo",
+					"expanded_url": "https://twitter.com/kredu02/status/1087437764486778880/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 557,
+							"h": 181,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 557,
+							"h": 181,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 557,
+							"h": 181,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 132141652,
+			"id_str": "132141652",
+			"name": "읽지 않을 논문 수집가",
+			"screen_name": "kredu02",
+			"location": "연구실",
+			"description": "매월 첫째주 수요일에 보름달이 뜨면 공부하는 대학원생",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 204,
+			"friends_count": 569,
+			"listed_count": 1,
+			"created_at": "Mon Apr 12 11:44:41 +0000 2010",
+			"favourites_count": 29462,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3105,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/614028533824749568/yHC07nfE_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/614028533824749568/yHC07nfE_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/132141652/1475376292",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 53,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오버워치 영웅 솔져:76이 동성애자라는 설정에 대한 나무위키의 서술. 이 땅의 모든 혐오는 장작위키로 모인다. https://t.co/1NIy5sMkHo"
+	},
+	{
 		"created_at": "Tue Jan 22 17:06:49 +0000 2019",
 		"id": 1087758494151766000,
 		"id_str": "1087758494151766018",
