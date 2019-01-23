e10219a5a3daf2f5a6225324e48cbae92cfaddf7
Yeoncomi <yeoncomi@gmail.com>
2019-01-22 20:17:08 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29123,
-	"created_at": "Wed Jan 23 03:17:08 +0000 2019",
+	"tweets": 29124,
+	"created_at": "Wed Jan 23 04:17:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 23 03:24:10 +0000 2019",
+		"id": 1087913855131430900,
+		"id_str": "1087919375997329408",
+		"full_text": "분류 - 국내 판타지 서적\n\n\"러시아 헌법\" https://t.co/mfRZnMLn8R",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			24
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1087913848173224000,
+					"id_str": "1087913848173223936",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/DxkL4YKX4AAiWtQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxkL4YKX4AAiWtQ.jpg",
+					"url": "https://t.co/mfRZnMLn8R",
+					"display_url": "pic.twitter.com/mfRZnMLn8R",
+					"expanded_url": "https://twitter.com/delfoass/status/1087913855131430913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 720,
+							"h": 369,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 349,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 369,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1087913848173224000,
+					"id_str": "1087913848173223936",
+					"indices": [
+						25,
+						48
+					],
+					"media_url": "http://pbs.twimg.com/media/DxkL4YKX4AAiWtQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxkL4YKX4AAiWtQ.jpg",
+					"url": "https://t.co/mfRZnMLn8R",
+					"display_url": "pic.twitter.com/mfRZnMLn8R",
+					"expanded_url": "https://twitter.com/delfoass/status/1087913855131430913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 720,
+							"h": 369,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 349,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 369,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 904406557,
+			"id_str": "904406557",
+			"name": "M1A2 SEP v2 Abrams",
+			"screen_name": "delfoass",
+			"location": "",
+			"description": "[/ [ ∧  ]I'◎:I[  ∧ ]＼]\nKorean male/Univ. Student/interested in @USARMY,@USASOC,#ABCT #TooHeavyForC130ToTransportMe #FuckingBigM153CROWSII",
+			"url": "https://t.co/LwDs4ozIU1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/LwDs4ozIU1",
+							"expanded_url": "http://blog.naver.com/delfosisyu",
+							"display_url": "blog.naver.com/delfosisyu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 500,
+			"friends_count": 820,
+			"listed_count": 6,
+			"created_at": "Thu Oct 25 18:15:17 +0000 2012",
+			"favourites_count": 6952,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 12651,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/851817983840210944/6Jzi_gPO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/851817983840210944/6Jzi_gPO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/904406557/1491924250",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 59,
+		"favorite_count": 23,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "분류 - 국내 판타지 서적\n\n\"러시아 헌법\" https://t.co/mfRZnMLn8R"
+	},
+	{
 		"created_at": "Tue Jan 22 09:46:51 +0000 2019",
 		"id": 1087647776026972200,
 		"id_str": "1087902376403423233",
