30981849b8a8e09c4702291a1072ec9341b1d381
Yeoncomi <yeoncomi@gmail.com>
2019-01-24 19:17:08 -0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29219,
-	"created_at": "Fri Jan 25 02:17:08 +0000 2019",
+	"tweets": 29221,
+	"created_at": "Fri Jan 25 03:17:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,286 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Fri Jan 25 02:27:44 +0000 2019",
+		"id": 1088624430098436100,
+		"id_str": "1088624430098436096",
+		"full_text": "yeoncomi\nツイート数:44(前日比:-27)\nRTした数:25(前日比:-5)\nRTされた数:1(前日比:-1)\n送ったリプライ数:10(前日比:-23)\n受け取ったリプライ数:9(前日比:-17)\nいいねされた数:4(前日比:+1)\nhttps://t.co/k4kBmDY1iR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			145
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/k4kBmDY1iR",
+					"expanded_url": "http://utabami.com/TodaysTwitterLife",
+					"display_url": "utabami.com/TodaysTwitterL…",
+					"indices": [
+						122,
+						145
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://utabami.com/TodaysTwitterLife/\" rel=\"nofollow\">今日のツイライフ</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 200682540,
+			"id_str": "200682540",
+			"name": "연꺼밍🎗🌈",
+			"screen_name": "yeoncomi",
+			"location": "Maple Ridge, British Columbia",
+			"description": "GMT -7(PDT) | 뒷계: @yeoncomi_NSFW|캐나다 유학생|개발도 합니다|파이선성애자|유학중이라서 트위터 잘 못합니다 |이 계정은 소속 단체/기관의 의견을 대변하지 않습니다. @v_anil_la|@MarshyPlay|#GirlsCanDoAnything",
+			"url": "https://t.co/9GnQQtk86E",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9GnQQtk86E",
+							"expanded_url": "https://yeonco.me",
+							"display_url": "yeonco.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 281,
+			"friends_count": 951,
+			"listed_count": 6,
+			"created_at": "Sat Oct 09 23:35:10 +0000 2010",
+			"favourites_count": 21507,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 31496,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme16/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1085901015495323652/jEJn4oS-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1085901015495323652/jEJn4oS-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/200682540/1499339251",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ja",
+		"text": "yeoncomi\nツイート数:44(前日比:-27)\nRTした数:25(前日比:-5)\nRTされた数:1(前日比:-1)\n送ったリプライ数:10(前日比:-23)\n受け取ったリプライ数:9(前日比:-17)\nいいねされた数:4(前日比:+1)\nhttps://t.co/k4kBmDY1iR"
+	},
+	{
+		"created_at": "Wed Jan 23 04:47:19 +0000 2019",
+		"id": 1087934781466263600,
+		"id_str": "1088623713098948611",
+		"full_text": "난 질외사정법 하면 이것밖에 안떠오르던데 https://t.co/boBYbU4GzA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			22
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1087934769999069200,
+					"id_str": "1087934769999069184",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/Dxke6MCXQAAjHqs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dxke6MCXQAAjHqs.jpg",
+					"url": "https://t.co/boBYbU4GzA",
+					"display_url": "pic.twitter.com/boBYbU4GzA",
+					"expanded_url": "https://twitter.com/Yu_haa_/status/1087934781466263554/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 930,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 395,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 930,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1087934769999069200,
+					"id_str": "1087934769999069184",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/Dxke6MCXQAAjHqs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dxke6MCXQAAjHqs.jpg",
+					"url": "https://t.co/boBYbU4GzA",
+					"display_url": "pic.twitter.com/boBYbU4GzA",
+					"expanded_url": "https://twitter.com/Yu_haa_/status/1087934781466263554/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 540,
+							"h": 930,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 395,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 540,
+							"h": 930,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 988002580805505000,
+			"id_str": "988002580805505024",
+			"name": "유하?",
+			"screen_name": "Yu_haa_",
+			"location": "대한민국 대구",
+			"description": "방구석 겜돌이",
+			"url": "https://t.co/n1VLly3iUk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/n1VLly3iUk",
+							"expanded_url": "https://ask.fm/Yu_haa_",
+							"display_url": "ask.fm/Yu_haa_",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 526,
+			"friends_count": 476,
+			"listed_count": 20,
+			"created_at": "Sun Apr 22 10:32:26 +0000 2018",
+			"favourites_count": 2054,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 33879,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1085909938847662082/zM92eCDa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1085909938847662082/zM92eCDa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/988002580805505024/1548239316",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2521,
+		"favorite_count": 488,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "난 질외사정법 하면 이것밖에 안떠오르던데 https://t.co/boBYbU4GzA"
+	},
+	{
 		"created_at": "Thu Jan 24 16:24:28 +0000 2019",
 		"id": 1088472613293158400,
 		"id_str": "1088619447500234752",
