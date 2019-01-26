97a6f78b3641cc573d11e01152bd32b41dccfb29
Yeoncomi <yeoncomi@gmail.com>
2019-01-25 23:17:07 -0800

    Added 26 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29267,
-	"created_at": "Sat Jan 26 06:17:09 +0000 2019",
+	"tweets": 29293,
+	"created_at": "Sat Jan 26 07:17:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_01.js",
 		"year": 2019,
 		"var_name": "tweets_2019_01",
-		"tweet_count": 995,
+		"tweet_count": 1021,
 		"month": 1
 	},
 	{
