4e2c39f786723aeeafe7e8e5cb6724c2eaf7e5be
Yeoncomi <yeoncomi@gmail.com>
2019-02-01 09:16:58 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29405,
-	"created_at": "Fri Feb 01 08:16:57 +0000 2019",
+	"tweets": 29406,
+	"created_at": "Fri Feb 01 17:16:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Fri Feb 01 16:55:14 +0000 2019",
+		"id": 1091379460744478700,
+		"id_str": "1091380875273547776",
+		"full_text": "We apologize to our customers who may be experiencing an issue with our online banking and mobile app.  Thanks for your patience while we research this issue.  If you are impacted, please check back here for updates.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			216
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://www.sprinklr.com\" rel=\"nofollow\">Sprinklr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 23002858,
+			"id_str": "23002858",
+			"name": "Wells Fargo",
+			"screen_name": "Ask_WellsFargo",
+			"location": "San Francisco, CA",
+			"description": "We’re here to listen, learn & lend a hand on Twitter 7 days a week. Ask us your banking questions & follow us for helpful tips & info about products & services.",
+			"url": "https://t.co/YVgAjkSv7p",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YVgAjkSv7p",
+							"expanded_url": "https://blogs.wf.com/community-guidelines/",
+							"display_url": "blogs.wf.com/community-guid…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60985,
+			"friends_count": 31542,
+			"listed_count": 738,
+			"created_at": "Fri Mar 06 00:16:32 +0000 2009",
+			"favourites_count": 2428,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 365919,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/463365241997230080/JNzSKBum_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/463365241997230080/JNzSKBum_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/23002858/1399330798",
+			"profile_link_color": "688FCF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "D7D3C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 22,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "We apologize to our customers who may be experiencing an issue with our online banking and mobile app.  Thanks for your patience while we research this issue.  If you are impacted, please check back here for updates."
+	},
+	{
 		"created_at": "Fri Feb 01 06:07:54 +0000 2019",
 		"id": 1091216551217311700,
 		"id_str": "1091248040256581632",
