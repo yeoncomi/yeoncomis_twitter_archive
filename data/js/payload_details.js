088e5ef14373898f077af6bc71b990bf2dfd2c81
Yeoncomi <yeoncomi@gmail.com>
2019-02-04 18:16:59 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29468,
-	"created_at": "Tue Feb 05 01:16:58 +0000 2019",
+	"tweets": 29469,
+	"created_at": "Tue Feb 05 02:16:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Mon Feb 04 10:02:12 +0000 2019",
+		"id": 1092362680642359300,
+		"id_str": "1092607389528862725",
+		"full_text": "페도새끼들 왜 여기 끼려하냐. https://t.co/gXt2G5jhL2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			16
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1092362673390379000,
+					"id_str": "1092362673390379008",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/DyjaEFlUYAAnPPx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyjaEFlUYAAnPPx.jpg",
+					"url": "https://t.co/gXt2G5jhL2",
+					"display_url": "pic.twitter.com/gXt2G5jhL2",
+					"expanded_url": "https://twitter.com/H_H_Funeral/status/1092362680642359301/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 638,
+							"h": 720,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 638,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 603,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092362673390379000,
+					"id_str": "1092362673390379008",
+					"indices": [
+						17,
+						40
+					],
+					"media_url": "http://pbs.twimg.com/media/DyjaEFlUYAAnPPx.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyjaEFlUYAAnPPx.jpg",
+					"url": "https://t.co/gXt2G5jhL2",
+					"display_url": "pic.twitter.com/gXt2G5jhL2",
+					"expanded_url": "https://twitter.com/H_H_Funeral/status/1092362680642359301/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 638,
+							"h": 720,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 638,
+							"h": 720,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 603,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3958494252,
+			"id_str": "3958494252",
+			"name": "하로하피퓨너럴",
+			"screen_name": "H_H_Funeral",
+			"location": "Girls Can Do Anything",
+			"description": "언제나 사악함을 추구하는 정신병자(조현증,양극성장애)/Extreme Metal/Bang Dream!/Broken by the Scream/DD오너(초보)/프사는 @M_ilkyway__ 님!",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 198,
+			"friends_count": 232,
+			"listed_count": 5,
+			"created_at": "Tue Oct 20 13:47:14 +0000 2015",
+			"favourites_count": 1013,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 31628,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1089940367221248000/tK0vKdAL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1089940367221248000/tK0vKdAL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3958494252/1543460633",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 233,
+		"favorite_count": 14,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "페도새끼들 왜 여기 끼려하냐. https://t.co/gXt2G5jhL2"
+	},
+	{
 		"created_at": "Tue Feb 05 01:11:00 +0000 2019",
 		"id": 1092591386312859600,
 		"id_str": "1092591763037061122",
