5a405d6eea095e717cea700ea6d0d8a3831a7343
Yeoncomi <yeoncomi@gmail.com>
2019-02-04 19:16:59 -0800

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Mon Feb 04 16:35:20 +0000 2019",
+		"id": 1092461616216461300,
+		"id_str": "1092611998532001792",
+		"full_text": "진짜 파네 ㅋㅋㅋㅋㅋㅋㅋ https://t.co/3D2PZiwzHi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			13
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1092461605848141800,
+					"id_str": "1092461605848141827",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/Dyk0CttU0AMRpqN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dyk0CttU0AMRpqN.jpg",
+					"url": "https://t.co/3D2PZiwzHi",
+					"display_url": "pic.twitter.com/3D2PZiwzHi",
+					"expanded_url": "https://twitter.com/Natsumi_LEN/status/1092461616216461313/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 331,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 996,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 584,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092461605848141800,
+					"id_str": "1092461605848141827",
+					"indices": [
+						14,
+						37
+					],
+					"media_url": "http://pbs.twimg.com/media/Dyk0CttU0AMRpqN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dyk0CttU0AMRpqN.jpg",
+					"url": "https://t.co/3D2PZiwzHi",
+					"display_url": "pic.twitter.com/3D2PZiwzHi",
+					"expanded_url": "https://twitter.com/Natsumi_LEN/status/1092461616216461313/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 331,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 996,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 584,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 194398385,
+			"id_str": "194398385",
+			"name": "夏美 (なつみ)",
+			"screen_name": "Natsumi_LEN",
+			"location": "대한민국 부천시",
+			"description": "TG / MtF / Cosplay / HRT💉1주년 /\n\n@KyoRenTOU💕",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1297,
+			"friends_count": 1120,
+			"listed_count": 18,
+			"created_at": "Fri Sep 24 01:12:56 +0000 2010",
+			"favourites_count": 20831,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 163358,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFF04D",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme19/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme19/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1091098971978620928/QOZNmAuz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1091098971978620928/QOZNmAuz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/194398385/1394896993",
+			"profile_link_color": "0099CC",
+			"profile_sidebar_border_color": "FFF8AD",
+			"profile_sidebar_fill_color": "F6FFD1",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "진짜 파네 ㅋㅋㅋㅋㅋㅋㅋ https://t.co/3D2PZiwzHi"
+	},
+	{
 		"created_at": "Mon Feb 04 10:02:12 +0000 2019",
 		"id": 1092362680642359300,
 		"id_str": "1092607389528862725",
