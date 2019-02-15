d8775723365a7c73c5b7f733cb628cc97cac6ee9
Yeoncomi <yeoncomi@gmail.com>
2019-02-15 09:17:01 -0800

    Added 1 tweets

--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Fri Feb 15 05:28:25 +0000 2019",
+		"id": 1096280046979801100,
+		"id_str": "1096453415050178561",
+		"full_text": "113년전 감기약 아편 + 헤로인 + 대마초 + 알콜 + 클로로포름\n숙주를 죽여서라도 목표 바이러스는 제거한다. https://t.co/qxW617A3ax",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			62
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1096280030315917300,
+					"id_str": "1096280030315917314",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/DzbE4RVVsAIKBHQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzbE4RVVsAIKBHQ.jpg",
+					"url": "https://t.co/qxW617A3ax",
+					"display_url": "pic.twitter.com/qxW617A3ax",
+					"expanded_url": "https://twitter.com/RikaTakanashi02/status/1096280046979801088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 430,
+							"h": 464,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 430,
+							"h": 464,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 464,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1096280030315917300,
+					"id_str": "1096280030315917314",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/DzbE4RVVsAIKBHQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzbE4RVVsAIKBHQ.jpg",
+					"url": "https://t.co/qxW617A3ax",
+					"display_url": "pic.twitter.com/qxW617A3ax",
+					"expanded_url": "https://twitter.com/RikaTakanashi02/status/1096280046979801088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 430,
+							"h": 464,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 430,
+							"h": 464,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 464,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1096280030269759500,
+					"id_str": "1096280030269759488",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/DzbE4RKVYAAqg42.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzbE4RKVYAAqg42.jpg",
+					"url": "https://t.co/qxW617A3ax",
+					"display_url": "pic.twitter.com/qxW617A3ax",
+					"expanded_url": "https://twitter.com/RikaTakanashi02/status/1096280046979801088/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 430,
+							"h": 336,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 430,
+							"h": 336,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 430,
+							"h": 336,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1002565948476280800,
+			"id_str": "1002565948476280832",
+			"name": "GIGA",
+			"screen_name": "RikaTakanashi02",
+			"location": "성남",
+			"description": "문을 열고 들어오시오",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 226,
+			"friends_count": 378,
+			"listed_count": 4,
+			"created_at": "Fri Jun 01 15:02:03 +0000 2018",
+			"favourites_count": 1620,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5448,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1096251603785461760/PMYv4tSo_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1096251603785461760/PMYv4tSo_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1002565948476280832/1550201726",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "113년전 감기약 아편 + 헤로인 + 대마초 + 알콜 + 클로로포름\n숙주를 죽여서라도 목표 바이러스는 제거한다. https://t.co/qxW617A3ax"
+	},
+	{
 		"created_at": "Fri Feb 15 13:19:05 +0000 2019",
 		"id": 1096398492048678900,
 		"id_str": "1096398492048678914",
