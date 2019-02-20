21919cc0677694c5280d5c2b16517f5832c2c6c3
Yeoncomi <yeoncomi@gmail.com>
2019-02-19 21:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29723,
-	"created_at": "Wed Feb 20 03:17:02 +0000 2019",
+	"tweets": 29724,
+	"created_at": "Wed Feb 20 05:17:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Tue Feb 19 08:54:51 +0000 2019",
+		"id": 1097781547837513700,
+		"id_str": "1098088741283885056",
+		"full_text": "인천 둥춘동 살인사건 범인이\n\n초등학생 칼로 죽이고 쓴 트윗 글.\n\n사람은 어디까지 미칠 수 있는 것 일까 https://t.co/mLUaWfT9WQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			59
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1097781530359812100,
+					"id_str": "1097781530359812096",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/DzwafE4U0AAAhtC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzwafE4U0AAAhtC.jpg",
+					"url": "https://t.co/mLUaWfT9WQ",
+					"display_url": "pic.twitter.com/mLUaWfT9WQ",
+					"expanded_url": "https://twitter.com/Dream_Phan/status/1097781547837513729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1097781530359812100,
+					"id_str": "1097781530359812096",
+					"indices": [
+						60,
+						83
+					],
+					"media_url": "http://pbs.twimg.com/media/DzwafE4U0AAAhtC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzwafE4U0AAAhtC.jpg",
+					"url": "https://t.co/mLUaWfT9WQ",
+					"display_url": "pic.twitter.com/mLUaWfT9WQ",
+					"expanded_url": "https://twitter.com/Dream_Phan/status/1097781547837513729/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 775611987359207400,
+			"id_str": "775611987359207424",
+			"name": "꿈판",
+			"screen_name": "Dream_Phan",
+			"location": "",
+			"description": "우럭회좀 사주세요",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40,
+			"friends_count": 46,
+			"listed_count": 0,
+			"created_at": "Tue Sep 13 08:28:02 +0000 2016",
+			"favourites_count": 240,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5085,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1097077925793296384/aMZEn-bb_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1097077925793296384/aMZEn-bb_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/775611987359207424/1550162637",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1857,
+		"favorite_count": 389,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "인천 둥춘동 살인사건 범인이\n\n초등학생 칼로 죽이고 쓴 트윗 글.\n\n사람은 어디까지 미칠 수 있는 것 일까 https://t.co/mLUaWfT9WQ"
+	},
+	{
 		"created_at": "Tue Feb 19 11:28:43 +0000 2019",
 		"id": 1097820269211574300,
 		"id_str": "1098045665005850625",
