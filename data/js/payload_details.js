af4126ea39458ec0b4cd291f12e929834e6af5d2
Yeoncomi <yeoncomi@gmail.com>
2019-02-21 07:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29739,
-	"created_at": "Thu Feb 21 05:17:02 +0000 2019",
+	"tweets": 29740,
+	"created_at": "Thu Feb 21 15:17:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Thu Feb 21 14:23:58 +0000 2019",
+		"id": 1098589148233982000,
+		"id_str": "1098593693320110081",
+		"full_text": "알았습니다. 그냥 ESNI나 기다리고 있을께요. https://t.co/mI3bXdFIk2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			26
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1098589136745771000,
+					"id_str": "1098589136745771008",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/media/Dz74_9HVsAAf-0k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dz74_9HVsAAf-0k.jpg",
+					"url": "https://t.co/mI3bXdFIk2",
+					"display_url": "pic.twitter.com/mI3bXdFIk2",
+					"expanded_url": "https://twitter.com/mnnhwng/status/1098589148233981952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 594,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 374,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 594,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1098589136745771000,
+					"id_str": "1098589136745771008",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/media/Dz74_9HVsAAf-0k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dz74_9HVsAAf-0k.jpg",
+					"url": "https://t.co/mI3bXdFIk2",
+					"display_url": "pic.twitter.com/mI3bXdFIk2",
+					"expanded_url": "https://twitter.com/mnnhwng/status/1098589148233981952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 594,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 374,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 594,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1098589136850731000,
+					"id_str": "1098589136850731009",
+					"indices": [
+						27,
+						50
+					],
+					"media_url": "http://pbs.twimg.com/media/Dz74_9gXQAE07Gs.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dz74_9gXQAE07Gs.jpg",
+					"url": "https://t.co/mI3bXdFIk2",
+					"display_url": "pic.twitter.com/mI3bXdFIk2",
+					"expanded_url": "https://twitter.com/mnnhwng/status/1098589148233981952/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 602,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 379,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 602,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 116992809,
+			"id_str": "116992809",
+			"name": ".",
+			"screen_name": "mnnhwng",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 260,
+			"friends_count": 90,
+			"listed_count": 15,
+			"created_at": "Wed Feb 24 05:55:08 +0000 2010",
+			"favourites_count": 855,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 20146,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1052735307110445056/Nw-moxhO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1052735307110445056/Nw-moxhO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/116992809/1539826631",
+			"profile_link_color": "151322",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 16,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "알았습니다. 그냥 ESNI나 기다리고 있을께요. https://t.co/mI3bXdFIk2"
+	},
+	{
 		"created_at": "Fri Nov 16 09:19:08 +0000 2018",
 		"id": 1063360812213915600,
 		"id_str": "1098439985726775296",
