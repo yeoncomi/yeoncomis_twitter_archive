abfdd9e8e8473a59f4cac556cbf8f20112ca43be
Yeoncomi <yeoncomi@gmail.com>
2019-02-23 21:17:03 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29772,
-	"created_at": "Sun Feb 24 04:17:01 +0000 2019",
+	"tweets": 29773,
+	"created_at": "Sun Feb 24 05:17:02 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sun Feb 24 04:47:50 +0000 2019",
+		"id": 1099531322794504200,
+		"id_str": "1099534647606669312",
+		"full_text": "https://t.co/eMHaNIOHhU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1099531219455291400,
+					"id_str": "1099531219455291392",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D0JR0YsVsAAX7LT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0JR0YsVsAAX7LT.jpg",
+					"url": "https://t.co/eMHaNIOHhU",
+					"display_url": "pic.twitter.com/eMHaNIOHhU",
+					"expanded_url": "https://twitter.com/Myuzeka/status/1099531322794504194/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 271,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 962,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 962,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1099531219455291400,
+					"id_str": "1099531219455291392",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D0JR0YsVsAAX7LT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0JR0YsVsAAX7LT.jpg",
+					"url": "https://t.co/eMHaNIOHhU",
+					"display_url": "pic.twitter.com/eMHaNIOHhU",
+					"expanded_url": "https://twitter.com/Myuzeka/status/1099531322794504194/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 271,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 962,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 962,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1099531305782435800,
+					"id_str": "1099531305782435842",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D0JR5aSVYAIgssi.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0JR5aSVYAIgssi.jpg",
+					"url": "https://t.co/eMHaNIOHhU",
+					"display_url": "pic.twitter.com/eMHaNIOHhU",
+					"expanded_url": "https://twitter.com/Myuzeka/status/1099531322794504194/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 319,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 563,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1837,
+							"h": 862,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2906090166,
+			"id_str": "2906090166",
+			"name": "myuzeka",
+			"screen_name": "Myuzeka",
+			"location": "PIX:K:A:A:2018073002",
+			"description": "the legacy continues / mc1.7.10~1.13.2 / jb prop~festo / msc ~1+1/2 / ..?",
+			"url": "https://t.co/6dLQEOyUVv",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6dLQEOyUVv",
+							"expanded_url": "https://p.eagate.573.jp/game/museca/msc_1half",
+							"display_url": "p.eagate.573.jp/game/museca/ms…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 42,
+			"friends_count": 113,
+			"listed_count": 1,
+			"created_at": "Fri Nov 21 13:53:00 +0000 2014",
+			"favourites_count": 90,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2079,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/957095926979416064/8NbLWIOT_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/957095926979416064/8NbLWIOT_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2906090166/1549135581",
+			"profile_link_color": "B60033",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/eMHaNIOHhU"
+	},
+	{
 		"created_at": "Sun Feb 24 04:07:25 +0000 2019",
 		"id": 1099521153998086100,
 		"id_str": "1099521153998086144",
