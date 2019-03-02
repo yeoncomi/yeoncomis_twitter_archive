599561bfa26153bb0869a98b4d09b09f4bcdc391
Yeoncomi <yeoncomi@gmail.com>
2019-03-01 23:16:58 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29818,
-	"created_at": "Sat Mar 02 06:16:57 +0000 2019",
+	"tweets": 29819,
+	"created_at": "Sat Mar 02 07:16:57 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sat Mar 02 06:29:51 +0000 2019",
+		"id": 1101451444006215700,
+		"id_str": "1101731325285785601",
+		"full_text": "에비뉴가 뭐니 새아버지라고 해야지 https://t.co/sZ7A8Z2Ldw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			18
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1101451434296406000,
+					"id_str": "1101451434296406016",
+					"indices": [
+						19,
+						42
+					],
+					"media_url": "http://pbs.twimg.com/media/D0kkPmaU8AALflm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0kkPmaU8AALflm.jpg",
+					"url": "https://t.co/sZ7A8Z2Ldw",
+					"display_url": "pic.twitter.com/sZ7A8Z2Ldw",
+					"expanded_url": "https://twitter.com/Lrlouren/status/1101451444006215680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 421,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 421,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1101451434296406000,
+					"id_str": "1101451434296406016",
+					"indices": [
+						19,
+						42
+					],
+					"media_url": "http://pbs.twimg.com/media/D0kkPmaU8AALflm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0kkPmaU8AALflm.jpg",
+					"url": "https://t.co/sZ7A8Z2Ldw",
+					"display_url": "pic.twitter.com/sZ7A8Z2Ldw",
+					"expanded_url": "https://twitter.com/Lrlouren/status/1101451444006215680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 382,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 421,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 421,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1039134665573990400,
+			"id_str": "1039134665573990400",
+			"name": "Louren",
+			"screen_name": "Lrlouren",
+			"location": "",
+			"description": "The 1975 Blur Pale Waves Rat Boy",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 235,
+			"friends_count": 228,
+			"listed_count": 1,
+			"created_at": "Mon Sep 10 12:53:05 +0000 2018",
+			"favourites_count": 9182,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 12665,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1099871407427575808/epc1QPRL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1099871407427575808/epc1QPRL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1039134665573990400/1549654143",
+			"profile_link_color": "DDA0DD",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2652,
+		"favorite_count": 573,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "에비뉴가 뭐니 새아버지라고 해야지 https://t.co/sZ7A8Z2Ldw"
+	},
+	{
 		"created_at": "Sat Mar 02 06:12:54 +0000 2019",
 		"id": 1101478354329337900,
 		"id_str": "1101727057782829056",
