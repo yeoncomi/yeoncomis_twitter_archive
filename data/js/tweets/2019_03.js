c92ffaa91563b97761aa3f5dda5fced5525f1a07
Yeoncomi <yeoncomi@gmail.com>
2019-03-05 19:16:59 -0800

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,188 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 06 02:38:26 +0000 2019",
+		"id": 1102924246160982000,
+		"id_str": "1103122637834735617",
+		"full_text": "https://t.co/69ft4pUhLR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1102924218256277500,
+					"id_str": "1102924218256277504",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1102924218256277504/pu/img/OlWVoy8C6EPgnlQY.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1102924218256277504/pu/img/OlWVoy8C6EPgnlQY.jpg",
+					"url": "https://t.co/69ft4pUhLR",
+					"display_url": "pic.twitter.com/69ft4pUhLR",
+					"expanded_url": "https://twitter.com/thememebookoc/status/1102924246160982017/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 320,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 320,
+							"h": 400,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 320,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1102924218256277500,
+					"id_str": "1102924218256277504",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1102924218256277504/pu/img/OlWVoy8C6EPgnlQY.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1102924218256277504/pu/img/OlWVoy8C6EPgnlQY.jpg",
+					"url": "https://t.co/69ft4pUhLR",
+					"display_url": "pic.twitter.com/69ft4pUhLR",
+					"expanded_url": "https://twitter.com/thememebookoc/status/1102924246160982017/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 320,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 320,
+							"h": 400,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 320,
+							"h": 400,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							5
+						],
+						"duration_millis": 7800,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1102924218256277504/pu/pl/-MF70LM_ZGKswBwO.m3u8?tag=6"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1102924218256277504/pu/vid/320x400/MEs-Ot4iIMFsXlUY.mp4?tag=6"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 975024183594901500,
+			"id_str": "975024183594901504",
+			"name": "The Meme Book",
+			"screen_name": "thememebookoc",
+			"location": "🅱️",
+			"description": "Disclaimer - We do not own all content that is upload to this account.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37658,
+			"friends_count": 4,
+			"listed_count": 64,
+			"created_at": "Sat Mar 17 15:00:55 +0000 2018",
+			"favourites_count": 16,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1729,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/975025185752539136/1ASvcGp4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/975025185752539136/1ASvcGp4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/975024183594901504/1521299093",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1009,
+		"favorite_count": 2276,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "und",
+		"text": "https://t.co/69ft4pUhLR"
+	},
+	{
 		"created_at": "Wed Mar 06 01:54:12 +0000 2019",
 		"id": 1102935653166211100,
 		"id_str": "1103111504004411392",
