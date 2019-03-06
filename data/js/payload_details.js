e44c419ef9fc962c19f37dde04146ab8a2d83d8b
Yeoncomi <yeoncomi@gmail.com>
2019-03-06 12:17:01 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29858,
-	"created_at": "Wed Mar 06 03:16:58 +0000 2019",
+	"tweets": 29859,
+	"created_at": "Wed Mar 06 20:17:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 06 20:06:39 +0000 2019",
+		"id": 1103293787420454900,
+		"id_str": "1103386431907495937",
+		"full_text": "디씨의 용독문주 https://t.co/CpK0N2bKv0",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			8
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1103293777622360000,
+					"id_str": "1103293777622360064",
+					"indices": [
+						9,
+						32
+					],
+					"media_url": "http://pbs.twimg.com/media/D0-v2GIUYAAoAiL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0-v2GIUYAAoAiL.jpg",
+					"url": "https://t.co/CpK0N2bKv0",
+					"display_url": "pic.twitter.com/CpK0N2bKv0",
+					"expanded_url": "https://twitter.com/336111/status/1103293787420454917/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 602,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 341,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1435,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1103293777622360000,
+					"id_str": "1103293777622360064",
+					"indices": [
+						9,
+						32
+					],
+					"media_url": "http://pbs.twimg.com/media/D0-v2GIUYAAoAiL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0-v2GIUYAAoAiL.jpg",
+					"url": "https://t.co/CpK0N2bKv0",
+					"display_url": "pic.twitter.com/CpK0N2bKv0",
+					"expanded_url": "https://twitter.com/336111/status/1103293787420454917/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 602,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 341,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1435,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 810965522,
+			"id_str": "810965522",
+			"name": "쿠즈쿠쿠다스 통칭밥솥",
+			"screen_name": "336111",
+			"location": "수원시 우크라이나 갈라파고스 제도 20번지 304호",
+			"description": "헛소리는 선거공약급 생각의전환은 우디르  신짱구의 처세술  센스는 o sorry 싫으면 블락하소 \n뮤트 할바에 블락\n 인장 @1311461K",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 387,
+			"friends_count": 1332,
+			"listed_count": 6,
+			"created_at": "Sat Sep 08 13:50:08 +0000 2012",
+			"favourites_count": 41306,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 276905,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1092812178401374209/33RlPPdL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1092812178401374209/33RlPPdL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/810965522/1468322210",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 350,
+		"favorite_count": 32,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "디씨의 용독문주 https://t.co/CpK0N2bKv0"
+	},
+	{
 		"created_at": "Wed Mar 06 02:38:26 +0000 2019",
 		"id": 1102924246160982000,
 		"id_str": "1103122637834735617",
