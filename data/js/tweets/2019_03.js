d710a4f8f354515ead36a4da8eb7b7b676ac9d60
Yeoncomi <yeoncomi@gmail.com>
2019-03-11 09:17:01 -0700

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Mon Mar 11 15:48:12 +0000 2019",
+		"id": 1105069030958760000,
+		"id_str": "1105133329001508864",
+		"full_text": "김도 방송하는 내내 싱글벙글\n\n핵 해명방송인데 해명된건 치핵이랑 앞머리에 떨어진 전술핵 뿐이었다. https://t.co/jXqwtgf0S6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			54
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1105068975426166800,
+					"id_str": "1105068975426166784",
+					"indices": [
+						55,
+						78
+					],
+					"media_url": "http://pbs.twimg.com/media/D1X-YNSU4AAb6tQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1X-YNSU4AAb6tQ.jpg",
+					"url": "https://t.co/jXqwtgf0S6",
+					"display_url": "pic.twitter.com/jXqwtgf0S6",
+					"expanded_url": "https://twitter.com/Sumire_12K/status/1105069030958759936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 299,
+							"h": 305,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 299,
+							"h": 305,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 299,
+							"h": 305,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1105068975426166800,
+					"id_str": "1105068975426166784",
+					"indices": [
+						55,
+						78
+					],
+					"media_url": "http://pbs.twimg.com/media/D1X-YNSU4AAb6tQ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1X-YNSU4AAb6tQ.jpg",
+					"url": "https://t.co/jXqwtgf0S6",
+					"display_url": "pic.twitter.com/jXqwtgf0S6",
+					"expanded_url": "https://twitter.com/Sumire_12K/status/1105069030958759936/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 299,
+							"h": 305,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 299,
+							"h": 305,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 299,
+							"h": 305,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 232113411,
+			"id_str": "232113411",
+			"name": "Sumire🔞",
+			"screen_name": "Sumire_12K",
+			"location": "Kor/Jpn 👌🏽",
+			"description": "Profic : @SeeUmai / Header : @SD_BigPie\n\nYes, Saiga-12",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2550,
+			"friends_count": 501,
+			"listed_count": 34,
+			"created_at": "Thu Dec 30 08:09:15 +0000 2010",
+			"favourites_count": 35058,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 191408,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1096440146050801664/0-SMfRoV_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1096440146050801664/0-SMfRoV_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/232113411/1547300100",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 86,
+		"favorite_count": 27,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "김도 방송하는 내내 싱글벙글\n\n핵 해명방송인데 해명된건 치핵이랑 앞머리에 떨어진 전술핵 뿐이었다. https://t.co/jXqwtgf0S6"
+	},
+	{
 		"created_at": "Mon Mar 11 14:51:25 +0000 2019",
 		"id": 1105119039670542300,
 		"id_str": "1105119039670542336",
