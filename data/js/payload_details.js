0671894fb6ec9c9d56143a403434cf1eeb129302
Yeoncomi <yeoncomi@gmail.com>
2019-03-12 22:17:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29919,
-	"created_at": "Wed Mar 13 03:16:58 +0000 2019",
+	"tweets": 29920,
+	"created_at": "Wed Mar 13 05:17:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 13 05:14:46 +0000 2019",
+		"id": 1104250559152840700,
+		"id_str": "1105698694706282496",
+		"full_text": "ㅋㅋㅋㅋ티웨이 항공 3행시 당첨작이\n\n티 : 티켓이 아무리 구하기 힘들어도\n웨 : 웨이팅이 길어도\n이 : 이스타 https://t.co/cU3A8esqwR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			62
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1104250550596464600,
+					"id_str": "1104250550596464640",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/D1MWBnNU8AARaKk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1MWBnNU8AARaKk.jpg",
+					"url": "https://t.co/cU3A8esqwR",
+					"display_url": "pic.twitter.com/cU3A8esqwR",
+					"expanded_url": "https://twitter.com/L__forest/status/1104250559152840704/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 618,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 618,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 560,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1104250550596464600,
+					"id_str": "1104250550596464640",
+					"indices": [
+						63,
+						86
+					],
+					"media_url": "http://pbs.twimg.com/media/D1MWBnNU8AARaKk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1MWBnNU8AARaKk.jpg",
+					"url": "https://t.co/cU3A8esqwR",
+					"display_url": "pic.twitter.com/cU3A8esqwR",
+					"expanded_url": "https://twitter.com/L__forest/status/1104250559152840704/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 750,
+							"h": 618,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 750,
+							"h": 618,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 560,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 949284279338418200,
+			"id_str": "949284279338418177",
+			"name": "서현수",
+			"screen_name": "L__forest",
+			"location": "",
+			"description": "저렇게 던져놔두 토마토가 열리더라.. 신기해",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 37,
+			"friends_count": 57,
+			"listed_count": 1,
+			"created_at": "Fri Jan 05 14:19:43 +0000 2018",
+			"favourites_count": 136,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 837,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1038739372680478720/Y8E5N9Xa_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1038739372680478720/Y8E5N9Xa_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/949284279338418177/1536489741",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2298,
+		"favorite_count": 451,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "ㅋㅋㅋㅋ티웨이 항공 3행시 당첨작이\n\n티 : 티켓이 아무리 구하기 힘들어도\n웨 : 웨이팅이 길어도\n이 : 이스타 https://t.co/cU3A8esqwR"
+	},
+	{
 		"created_at": "Wed Mar 13 02:26:47 +0000 2019",
 		"id": 1105656421943636000,
 		"id_str": "1105656421943635969",
