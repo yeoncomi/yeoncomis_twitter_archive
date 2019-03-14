58b1d1a2b215a19f09355feea75a951cc8863260
Yeoncomi <yeoncomi@gmail.com>
2019-03-14 14:16:59 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 29935,
-	"created_at": "Thu Mar 14 14:17:00 +0000 2019",
+	"tweets": 29936,
+	"created_at": "Thu Mar 14 21:16:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,222 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Thu Mar 14 20:51:41 +0000 2019",
+		"id": 1106289143909367800,
+		"id_str": "1106296867178975232",
+		"full_text": "Everything you need to know to watch @TheJUNOAwards this Sunday: https://t.co/IL5VkQjm4X #JUNOS https://t.co/atqbTVTU4o",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			95
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "JUNOS",
+					"indices": [
+						89,
+						95
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TheJUNOAwards",
+					"name": "The JUNO Awards",
+					"id": 24705687,
+					"id_str": "24705687",
+					"indices": [
+						37,
+						51
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/IL5VkQjm4X",
+					"expanded_url": "https://www.cbc.ca/music/junos/schedule/how-to-watch-the-2019-juno-awards-1.4989611",
+					"display_url": "cbc.ca/music/junos/sc…",
+					"indices": [
+						65,
+						88
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1106203377745252400,
+					"id_str": "1106203377745252353",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D1oGHHPX0AERNYh.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D1oGHHPX0AERNYh.jpg",
+					"url": "https://t.co/atqbTVTU4o",
+					"display_url": "pic.twitter.com/atqbTVTU4o",
+					"expanded_url": "https://twitter.com/CBC/status/1106289143909367808/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 1082,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1082,
+							"h": 1082,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1106203377745252400,
+					"id_str": "1106203377745252353",
+					"indices": [
+						96,
+						119
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D1oGHHPX0AERNYh.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D1oGHHPX0AERNYh.jpg",
+					"url": "https://t.co/atqbTVTU4o",
+					"display_url": "pic.twitter.com/atqbTVTU4o",
+					"expanded_url": "https://twitter.com/CBC/status/1106289143909367808/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1082,
+							"h": 1082,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1082,
+							"h": 1082,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							1,
+							1
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D1oGHHPX0AERNYh.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16034244,
+			"id_str": "16034244",
+			"name": "CBC",
+			"screen_name": "CBC",
+			"location": "Canada",
+			"description": "Canadian Broadcasting Corporation",
+			"url": "http://t.co/r6FyHQK69r",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/r6FyHQK69r",
+							"expanded_url": "http://www.cbc.ca",
+							"display_url": "cbc.ca",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 444065,
+			"friends_count": 83048,
+			"listed_count": 2947,
+			"created_at": "Thu Aug 28 23:08:39 +0000 2008",
+			"favourites_count": 41750,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 134042,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "BB1500",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1068187817375932417/GvePoLQ7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1068187817375932417/GvePoLQ7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16034244/1552490888",
+			"profile_link_color": "0073FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "0C3E53",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 4,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Everything you need to know to watch @TheJUNOAwards this Sunday: https://t.co/IL5VkQjm4X #JUNOS https://t.co/atqbTVTU4o"
+	},
+	{
 		"created_at": "Thu Mar 14 13:44:33 +0000 2019",
 		"id": 1106126436597784600,
 		"id_str": "1106189374822113288",
