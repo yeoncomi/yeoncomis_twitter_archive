227d9f25ccb47a28195fd754525200f71ecfd4fe
Yeoncomi <yeoncomi@gmail.com>
2019-03-18 17:17:00 -0700

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Mon Mar 18 23:20:44 +0000 2019",
+		"id": 1107623111329890300,
+		"id_str": "1107783926536261632",
+		"full_text": "신호등 치킨 게섯거라 https://t.co/txN8PE45eJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			11
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1107623053041635300,
+					"id_str": "1107623053041635330",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/D18RTF2UkAISeZz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D18RTF2UkAISeZz.jpg",
+					"url": "https://t.co/txN8PE45eJ",
+					"display_url": "pic.twitter.com/txN8PE45eJ",
+					"expanded_url": "https://twitter.com/miskyym/status/1107623111329890304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 773,
+							"h": 1093,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 773,
+							"h": 1093,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1107623053041635300,
+					"id_str": "1107623053041635330",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/D18RTF2UkAISeZz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D18RTF2UkAISeZz.jpg",
+					"url": "https://t.co/txN8PE45eJ",
+					"display_url": "pic.twitter.com/txN8PE45eJ",
+					"expanded_url": "https://twitter.com/miskyym/status/1107623111329890304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 481,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 773,
+							"h": 1093,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 773,
+							"h": 1093,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1107623104337985500,
+					"id_str": "1107623104337985537",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/D18RWE8UwAE9USp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D18RWE8UwAE9USp.jpg",
+					"url": "https://t.co/txN8PE45eJ",
+					"display_url": "pic.twitter.com/txN8PE45eJ",
+					"expanded_url": "https://twitter.com/miskyym/status/1107623111329890304/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 850,
+							"h": 637,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 850,
+							"h": 637,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 766100459656536000,
+			"id_str": "766100459656536065",
+			"name": "격언P",
+			"screen_name": "miskyym",
+			"location": "",
+			"description": "이러한 격언을 알고 계시는지?",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 654,
+			"friends_count": 638,
+			"listed_count": 7,
+			"created_at": "Thu Aug 18 02:32:37 +0000 2016",
+			"favourites_count": 15829,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 39795,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1029726696188956673/Tj3U72af_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1029726696188956673/Tj3U72af_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/766100459656536065/1534366269",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 37,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "신호등 치킨 게섯거라 https://t.co/txN8PE45eJ"
+	},
+	{
 		"created_at": "Mon Mar 18 21:08:20 +0000 2019",
 		"id": 1107750607153037300,
 		"id_str": "1107750607153037312",
