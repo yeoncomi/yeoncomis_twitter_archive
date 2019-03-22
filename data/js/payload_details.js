8164de40f64c75b59f315701f5e7d442501b1f47
Yeoncomi <yeoncomi@gmail.com>
2019-03-21 21:17:01 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30127,
-	"created_at": "Fri Mar 22 03:17:01 +0000 2019",
+	"tweets": 30128,
+	"created_at": "Fri Mar 22 04:17:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,179 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Fri Mar 22 03:51:43 +0000 2019",
+		"id": 1108929893918609400,
+		"id_str": "1108939286856908800",
+		"full_text": "트이타 창사 13주년 기념파티를 한국에서... 오늘이 창사기념일이라고... https://t.co/yiosIgMTWo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1108929884003299300,
+					"id_str": "1108929884003299329",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/D2O12qjU8AEpccT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2O12qjU8AEpccT.jpg",
+					"url": "https://t.co/yiosIgMTWo",
+					"display_url": "pic.twitter.com/yiosIgMTWo",
+					"expanded_url": "https://twitter.com/sanwang78/status/1108929893918609415/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1108929884003299300,
+					"id_str": "1108929884003299329",
+					"indices": [
+						42,
+						65
+					],
+					"media_url": "http://pbs.twimg.com/media/D2O12qjU8AEpccT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2O12qjU8AEpccT.jpg",
+					"url": "https://t.co/yiosIgMTWo",
+					"display_url": "pic.twitter.com/yiosIgMTWo",
+					"expanded_url": "https://twitter.com/sanwang78/status/1108929893918609415/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 105048824,
+			"id_str": "105048824",
+			"name": "산왕",
+			"screen_name": "sanwang78",
+			"location": "Seoul,Korea",
+			"description": "Otaking & Team Leader, Gamefocus",
+			"url": "https://t.co/9Ew4QYmKFw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9Ew4QYmKFw",
+							"expanded_url": "http://sanwang78.egloos.com",
+							"display_url": "sanwang78.egloos.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3337,
+			"friends_count": 1690,
+			"listed_count": 87,
+			"created_at": "Fri Jan 15 05:40:56 +0000 2010",
+			"favourites_count": 1696,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 155842,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1105013632142729216/p-9BrgkM_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1105013632142729216/p-9BrgkM_normal.png",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 601,
+		"favorite_count": 73,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "트이타 창사 13주년 기념파티를 한국에서... 오늘이 창사기념일이라고... https://t.co/yiosIgMTWo"
+	},
+	{
 		"created_at": "Fri Mar 22 02:27:42 +0000 2019",
 		"id": 1108918143584358400,
 		"id_str": "1108918143584358400",
