a1ffd3527a0fbe19f9e6e775209cb80255c1610b
Yeoncomi <yeoncomi@gmail.com>
2019-03-28 14:19:14 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30351,
-	"created_at": "Thu Mar 28 20:17:04 +0000 2019",
+	"tweets": 30352,
+	"created_at": "Thu Mar 28 21:19:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,244 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Thu Mar 28 20:23:06 +0000 2019",
+		"id": 1110979004553666600,
+		"id_str": "1111363103797596161",
+		"full_text": "This is a 50 second video that felt like it was four hours long. https://t.co/82tNt1yzgX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1110978812395864000,
+					"id_str": "1110978812395864064",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1110978812395864064/pu/img/fXC0tBOkwQ55scsF.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1110978812395864064/pu/img/fXC0tBOkwQ55scsF.jpg",
+					"url": "https://t.co/82tNt1yzgX",
+					"display_url": "pic.twitter.com/82tNt1yzgX",
+					"expanded_url": "https://twitter.com/xocelotyouth/status/1110979004553666566/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1110978812395864000,
+					"id_str": "1110978812395864064",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1110978812395864064/pu/img/fXC0tBOkwQ55scsF.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1110978812395864064/pu/img/fXC0tBOkwQ55scsF.jpg",
+					"url": "https://t.co/82tNt1yzgX",
+					"display_url": "pic.twitter.com/82tNt1yzgX",
+					"expanded_url": "https://twitter.com/xocelotyouth/status/1110979004553666566/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 49770,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1110978812395864064/pu/vid/1280x720/GEOIuMlDtRuvGJXB.mp4?tag=8"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1110978812395864064/pu/pl/boYebFqAjAW_20Ls.m3u8?tag=8"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1110978812395864064/pu/vid/640x360/VqsKQ8q0Pyrsfk16.mp4?tag=8"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1110978812395864064/pu/vid/320x180/23zo0vORLzdN5CU4.mp4?tag=8"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 704841870237503500,
+			"id_str": "704841870237503489",
+			"name": "FP | ocelotyouth",
+			"screen_name": "xocelotyouth",
+			"location": "Killumbus, OH",
+			"description": "Pretty ok at Street Fighter • Birdie/Vega • @litebritejpg💍 • Owner of @frameperfectfgc",
+			"url": "https://t.co/xDs2eNT8bF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/xDs2eNT8bF",
+							"expanded_url": "http://twitch.tv/ocelotyouthx",
+							"display_url": "twitch.tv/ocelotyouthx",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 501,
+			"friends_count": 657,
+			"listed_count": 3,
+			"created_at": "Wed Mar 02 01:32:51 +0000 2016",
+			"favourites_count": 19703,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10343,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1078814452366094338/6zZrZIGk_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1078814452366094338/6zZrZIGk_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/704841870237503489/1550700789",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "3df0e3eb1e91170b",
+			"url": "https://api.twitter.com/1.1/geo/id/3df0e3eb1e91170b.json",
+			"place_type": "city",
+			"name": "Columbus",
+			"full_name": "Columbus, OH",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-83.200169,
+							39.832013
+						],
+						[
+							-82.7713751,
+							39.832013
+						],
+						[
+							-82.7713751,
+							40.157354
+						],
+						[
+							-83.200169,
+							40.157354
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2724,
+		"favorite_count": 7388,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is a 50 second video that felt like it was four hours long. https://t.co/82tNt1yzgX"
+	},
+	{
 		"created_at": "Thu Mar 28 20:09:43 +0000 2019",
 		"id": 1111253158964199400,
 		"id_str": "1111359734756433920",
