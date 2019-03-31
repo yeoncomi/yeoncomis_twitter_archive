58451919fceb7d15dac702a912dbe89e751c26fd
Yeoncomi <yeoncomi@gmail.com>
2019-03-31 06:17:04 -0700

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sun Mar 31 13:16:46 +0000 2019",
+		"id": 1112337913730072600,
+		"id_str": "1112342974313218048",
+		"full_text": "어제 이거 보고 redis-covered가 뭐지..? 하고 잠깐 고민했다 ㅋㅋ https://t.co/41Jjg52juz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			43
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1112337903462408200,
+					"id_str": "1112337903462408192",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/D2_RbgYUYAA2Ll7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2_RbgYUYAA2Ll7.jpg",
+					"url": "https://t.co/41Jjg52juz",
+					"display_url": "pic.twitter.com/41Jjg52juz",
+					"expanded_url": "https://twitter.com/redomichi/status/1112337913730072577/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1112337903462408200,
+					"id_str": "1112337903462408192",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/D2_RbgYUYAA2Ll7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2_RbgYUYAA2Ll7.jpg",
+					"url": "https://t.co/41Jjg52juz",
+					"display_url": "pic.twitter.com/41Jjg52juz",
+					"expanded_url": "https://twitter.com/redomichi/status/1112337913730072577/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3018162187,
+			"id_str": "3018162187",
+			"name": "가상두부",
+			"screen_name": "redomichi",
+			"location": "",
+			"description": "프로그래머입니다. 저는 귀엽다. 좋아하는 거는 프로그래머 유머, 자동화, 놀고먹기, 술, 차, 인디음악, 중식 일식 그리고\n뒷계 @virtual_tofu",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 241,
+			"friends_count": 483,
+			"listed_count": 3,
+			"created_at": "Fri Feb 13 06:46:27 +0000 2015",
+			"favourites_count": 5125,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 15659,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1111983836324192257/w-1tE-sB_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1111983836324192257/w-1tE-sB_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3018162187/1553946622",
+			"profile_link_color": "F58EA8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 17,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "어제 이거 보고 redis-covered가 뭐지..? 하고 잠깐 고민했다 ㅋㅋ https://t.co/41Jjg52juz"
+	},
+	{
 		"created_at": "Sun Mar 31 09:05:53 +0000 2019",
 		"id": 1112166440004522000,
 		"id_str": "1112279838906212352",
@@ -605,7 +766,7 @@ Grailbird.data.tweets_2019_03 =
 		"user": {
 			"id": 1000709531205124100,
 			"id_str": "1000709531205124096",
-			"name": "아​​​​저​​​​​거​​​또",
+			"name": "아저거또",
 			"screen_name": "AzeogeottO",
 			"location": "포도농장",
 			"description": "그림쟁이(하고싶다) || https://t.co/L8Sb3ekx4Z",
