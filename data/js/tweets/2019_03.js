5cd301386f751c9ccf038597be332222b9ff6af5
Yeoncomi <yeoncomi@gmail.com>
2019-03-31 14:17:06 -0700

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,249 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sun Mar 31 20:31:58 +0000 2019",
+		"id": 1112438980530692100,
+		"id_str": "1112452498009157632",
+		"full_text": "오늘자 시발놈\n손절완료 https://t.co/dRYDZP79Vx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			12
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1112438965146017800,
+					"id_str": "1112438965146017792",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/D3AtWEfU4AAOPFb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3AtWEfU4AAOPFb.jpg",
+					"url": "https://t.co/dRYDZP79Vx",
+					"display_url": "pic.twitter.com/dRYDZP79Vx",
+					"expanded_url": "https://twitter.com/cleanloveliver/status/1112438980530692101/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1296,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 612,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1112438965146017800,
+					"id_str": "1112438965146017792",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/D3AtWEfU4AAOPFb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3AtWEfU4AAOPFb.jpg",
+					"url": "https://t.co/dRYDZP79Vx",
+					"display_url": "pic.twitter.com/dRYDZP79Vx",
+					"expanded_url": "https://twitter.com/cleanloveliver/status/1112438980530692101/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1296,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 612,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1112438973379444700,
+					"id_str": "1112438973379444736",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/D3AtWjKVAAAvWM_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3AtWjKVAAAvWM_.jpg",
+					"url": "https://t.co/dRYDZP79Vx",
+					"display_url": "pic.twitter.com/dRYDZP79Vx",
+					"expanded_url": "https://twitter.com/cleanloveliver/status/1112438980530692101/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1443,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1198,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 679,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 215111653,
+			"id_str": "215111653",
+			"name": "콧코로마망~",
+			"screen_name": "cleanloveliver",
+			"location": "대한민국 원주시",
+			"description": "민나 나카요시 시마쇼네-",
+			"url": "https://t.co/SSvHxNSSpz",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/SSvHxNSSpz",
+							"expanded_url": "http://gallog.dcinside.com/hotcool380",
+							"display_url": "gallog.dcinside.com/hotcool380",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 550,
+			"friends_count": 496,
+			"listed_count": 9,
+			"created_at": "Sat Nov 13 01:10:23 +0000 2010",
+			"favourites_count": 4095,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 87055,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1110590218313228289/VC_jt9p4_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1110590218313228289/VC_jt9p4_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/215111653/1551555696",
+			"profile_link_color": "698CE8",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "01fc1c695b07d838",
+			"url": "https://api.twitter.com/1.1/geo/id/01fc1c695b07d838.json",
+			"place_type": "city",
+			"name": "Wonju-si",
+			"full_name": "Wonju-si, Republic of Korea",
+			"country_code": "KR",
+			"country": "Republic of Korea",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							127.744388345108,
+							37.1412820541627
+						],
+						[
+							128.218184318104,
+							37.1412820541627
+						],
+						[
+							128.218184318104,
+							37.5132663998339
+						],
+						[
+							127.744388345108,
+							37.5132663998339
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "오늘자 시발놈\n손절완료 https://t.co/dRYDZP79Vx"
+	},
+	{
 		"created_at": "Sun Mar 31 17:34:04 +0000 2019",
 		"id": 1112285521621860400,
 		"id_str": "1112407727391662085",
