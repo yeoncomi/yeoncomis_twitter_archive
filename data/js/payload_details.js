6c2ad0c869ef4162dcb772df467280d582b318e8
Yeoncomi <yeoncomi@gmail.com>
2019-03-31 17:17:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30510,
-	"created_at": "Sun Mar 31 23:17:04 +0000 2019",
+	"tweets": 30511,
+	"created_at": "Mon Apr 01 00:17:03 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_04.js
@@ -0,0 +1,249 @@
+Grailbird.data.tweets_2019_04 = 
+[
+	{
+		"created_at": "Mon Apr 01 00:07:36 +0000 2019",
+		"id": 1112504108957655000,
+		"id_str": "1112506765445234688",
+		"full_text": "트위터 3컷 만화 *NEW* https://t.co/2MsvLpr7Kb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			15
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1112504019883167700,
+					"id_str": "1112504019883167745",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D3BogwRUkAEw_pC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3BogwRUkAEw_pC.jpg",
+					"url": "https://t.co/2MsvLpr7Kb",
+					"display_url": "pic.twitter.com/2MsvLpr7Kb",
+					"expanded_url": "https://twitter.com/kingkenny1967/status/1112504108957655040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1112504019883167700,
+					"id_str": "1112504019883167745",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D3BogwRUkAEw_pC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3BogwRUkAEw_pC.jpg",
+					"url": "https://t.co/2MsvLpr7Kb",
+					"display_url": "pic.twitter.com/2MsvLpr7Kb",
+					"expanded_url": "https://twitter.com/kingkenny1967/status/1112504108957655040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1112504043157372900,
+					"id_str": "1112504043157372928",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D3BoiG-UwAAfabM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3BoiG-UwAAfabM.jpg",
+					"url": "https://t.co/2MsvLpr7Kb",
+					"display_url": "pic.twitter.com/2MsvLpr7Kb",
+					"expanded_url": "https://twitter.com/kingkenny1967/status/1112504108957655040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 749,
+							"h": 1043,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 749,
+							"h": 1043,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 488,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1112504057556402200,
+					"id_str": "1112504057556402176",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D3Boi8nUgAAn9GD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3Boi8nUgAAn9GD.jpg",
+					"url": "https://t.co/2MsvLpr7Kb",
+					"display_url": "pic.twitter.com/2MsvLpr7Kb",
+					"expanded_url": "https://twitter.com/kingkenny1967/status/1112504108957655040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 602,
+							"h": 607,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 602,
+							"h": 607,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 602,
+							"h": 607,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 121141769,
+			"id_str": "121141769",
+			"name": "Dumbarton Tam",
+			"screen_name": "kingkenny1967",
+			"location": "Melbourne, Victoria",
+			"description": "a Melburnian.",
+			"url": "https://t.co/hDcKf7ltbK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/hDcKf7ltbK",
+							"expanded_url": "http://Instagram.com/kingkenny1967",
+							"display_url": "Instagram.com/kingkenny1967",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3688,
+			"friends_count": 2598,
+			"listed_count": 93,
+			"created_at": "Mon Mar 08 15:10:30 +0000 2010",
+			"favourites_count": 12135,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 37878,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "E7EDF0",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/792293015951933440/5mThYqd6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/792293015951933440/5mThYqd6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/121141769/1544353141",
+			"profile_link_color": "9266CC",
+			"profile_sidebar_border_color": "0C8210",
+			"profile_sidebar_fill_color": "00B32A",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "트위터 3컷 만화 *NEW* https://t.co/2MsvLpr7Kb"
+	}
+]
