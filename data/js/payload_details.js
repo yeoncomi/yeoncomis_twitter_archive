64566fa949ca6bb04d0b2a74e92afc67523e55e9
Yeoncomi <yeoncomi@gmail.com>
2019-04-05 11:16:59 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30604,
-	"created_at": "Fri Apr 05 11:16:59 +0000 2019",
+	"tweets": 30605,
+	"created_at": "Fri Apr 05 18:16:58 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Fri Apr 05 18:14:56 +0000 2019",
+		"id": 1114088117605023700,
+		"id_str": "1114229951044055040",
+		"full_text": "샷건아, 이거 니친구 아니냐?(2) https://t.co/SGMkCO2N49",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			19
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1114088105269534700,
+					"id_str": "1114088105269534720",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/media/D3YJOqMUIAABopJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3YJOqMUIAABopJ.jpg",
+					"url": "https://t.co/SGMkCO2N49",
+					"display_url": "pic.twitter.com/SGMkCO2N49",
+					"expanded_url": "https://twitter.com/mchldjrznsk2/status/1114088117605023744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 668,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1466,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1179,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1114088105269534700,
+					"id_str": "1114088105269534720",
+					"indices": [
+						20,
+						43
+					],
+					"media_url": "http://pbs.twimg.com/media/D3YJOqMUIAABopJ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3YJOqMUIAABopJ.jpg",
+					"url": "https://t.co/SGMkCO2N49",
+					"display_url": "pic.twitter.com/SGMkCO2N49",
+					"expanded_url": "https://twitter.com/mchldjrznsk2/status/1114088117605023744/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 668,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1440,
+							"h": 1466,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1179,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3302241236,
+			"id_str": "3302241236",
+			"name": "경로적분",
+			"screen_name": "mchldjrznsk2",
+			"location": "",
+			"description": "#MGGA",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 346,
+			"friends_count": 693,
+			"listed_count": 6,
+			"created_at": "Fri Jul 31 08:13:47 +0000 2015",
+			"favourites_count": 10168,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 36839,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112902435482218496/qpG6__7N_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112902435482218496/qpG6__7N_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3302241236/1554172251",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 33,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "샷건아, 이거 니친구 아니냐?(2) https://t.co/SGMkCO2N49"
+	},
+	{
 		"created_at": "Fri Apr 05 10:32:14 +0000 2019",
 		"id": 1114007923707895800,
 		"id_str": "1114113507547418625",
