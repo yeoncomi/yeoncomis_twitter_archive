08eb8034d90287e7c50d76d9ea6d4d1d777ccc10
Yeoncomi <yeoncomi@gmail.com>
2019-04-09 16:17:02 -0700

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Tue Apr 09 22:26:24 +0000 2019",
+		"id": 1115648142890766300,
+		"id_str": "1115742784936263680",
+		"full_text": "Eclipse...? https://t.co/s9zYHwjaid",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			11
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1115648131201286100,
+					"id_str": "1115648131201286145",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/D3uUEHqUwAEzINF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3uUEHqUwAEzINF.jpg",
+					"url": "https://t.co/s9zYHwjaid",
+					"display_url": "pic.twitter.com/s9zYHwjaid",
+					"expanded_url": "https://twitter.com/programmer_pic/status/1115648142890766338/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 607,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1023,
+							"h": 913,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1023,
+							"h": 913,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1115648131201286100,
+					"id_str": "1115648131201286145",
+					"indices": [
+						12,
+						35
+					],
+					"media_url": "http://pbs.twimg.com/media/D3uUEHqUwAEzINF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3uUEHqUwAEzINF.jpg",
+					"url": "https://t.co/s9zYHwjaid",
+					"display_url": "pic.twitter.com/s9zYHwjaid",
+					"expanded_url": "https://twitter.com/programmer_pic/status/1115648142890766338/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 607,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1023,
+							"h": 913,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1023,
+							"h": 913,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1097088021667602400,
+			"id_str": "1097088021667602432",
+			"name": "프로그래밍 드립",
+			"screen_name": "programmer_pic",
+			"location": "",
+			"description": "갤러리를 털어서 IT계열 드립, 짤들을 비정기적으로 올립니다. \\n 제보 받습니다 DM || 태그해주시면 어디든지 찾아가요 :)",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 993,
+			"friends_count": 1,
+			"listed_count": 9,
+			"created_at": "Sun Feb 17 10:59:02 +0000 2019",
+			"favourites_count": 85,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 82,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112719176475377665/Q9Tt5j61_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112719176475377665/Q9Tt5j61_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1097088021667602432/1550468526",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 31,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Eclipse...? https://t.co/s9zYHwjaid"
+	},
+	{
 		"created_at": "Tue Apr 09 18:31:38 +0000 2019",
 		"id": 1115616652052185100,
 		"id_str": "1115683704829952000",
