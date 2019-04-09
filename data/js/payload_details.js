9e3594622ffcd0a5fd03b7022cceed5f1bc06e75
Yeoncomi <yeoncomi@gmail.com>
2019-04-09 06:17:03 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30804,
-	"created_at": "Tue Apr 09 12:17:01 +0000 2019",
+	"tweets": 30805,
+	"created_at": "Tue Apr 09 13:17:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Tue Apr 09 12:23:06 +0000 2019",
+		"id": 1115554917303717900,
+		"id_str": "1115590961378107392",
+		"full_text": "감옥도 가나요? https://t.co/Ks81fKu04c",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			8
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1115554890678280200,
+					"id_str": "1115554890678280192",
+					"indices": [
+						9,
+						32
+					],
+					"media_url": "http://pbs.twimg.com/media/D3s_QzpUIAAVoRD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3s_QzpUIAAVoRD.jpg",
+					"url": "https://t.co/Ks81fKu04c",
+					"display_url": "pic.twitter.com/Ks81fKu04c",
+					"expanded_url": "https://twitter.com/cooker_libre/status/1115554917303717888/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 516,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1424,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 910,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1115554890678280200,
+					"id_str": "1115554890678280192",
+					"indices": [
+						9,
+						32
+					],
+					"media_url": "http://pbs.twimg.com/media/D3s_QzpUIAAVoRD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3s_QzpUIAAVoRD.jpg",
+					"url": "https://t.co/Ks81fKu04c",
+					"display_url": "pic.twitter.com/Ks81fKu04c",
+					"expanded_url": "https://twitter.com/cooker_libre/status/1115554917303717888/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 516,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1424,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 910,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 712644853692125200,
+			"id_str": "712644853692125185",
+			"name": "쿠커@리브레위키(料理の神様)",
+			"screen_name": "cooker_libre",
+			"location": "리브레위키 ",
+			"description": "리브레위키 협동조합 소속. 교회다니는 무신론자.소녀전선  ID 1188898\n프로필@daydreamic_kang\n\n비팔로우 알림 껐습니다.",
+			"url": "https://t.co/9HtuOFHmcu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9HtuOFHmcu",
+							"expanded_url": "http://librewiki.net",
+							"display_url": "librewiki.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 510,
+			"friends_count": 1091,
+			"listed_count": 11,
+			"created_at": "Wed Mar 23 14:19:08 +0000 2016",
+			"favourites_count": 29934,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 398183,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112744411794796545/Rl_1YlNF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112744411794796545/Rl_1YlNF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/712644853692125185/1543930942",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 446,
+		"favorite_count": 44,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "감옥도 가나요? https://t.co/Ks81fKu04c"
+	},
+	{
 		"created_at": "Tue Apr 09 12:14:28 +0000 2019",
 		"id": 1115587527665053700,
 		"id_str": "1115588789777260545",
