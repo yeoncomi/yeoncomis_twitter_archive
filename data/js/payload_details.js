0e10984d2f8b68c2e1a1ffbcd32104a7878369a5
Yeoncomi <yeoncomi@gmail.com>
2019-04-10 11:17:02 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30849,
-	"created_at": "Wed Apr 10 06:17:01 +0000 2019",
+	"tweets": 30850,
+	"created_at": "Wed Apr 10 18:17:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 10 17:41:16 +0000 2019",
+		"id": 1116032704720453600,
+		"id_str": "1116033416774934528",
+		"full_text": "https://t.co/zsyBefQd2E",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1116032694570311700,
+					"id_str": "1116032694570311680",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D3zx0p2V4AAhJ8f.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D3zx0p2V4AAhJ8f.jpg",
+					"url": "https://t.co/zsyBefQd2E",
+					"display_url": "pic.twitter.com/zsyBefQd2E",
+					"expanded_url": "https://twitter.com/wjsejguq_/status/1116032704720453632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 424,
+							"h": 318,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 424,
+							"h": 318,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 424,
+							"h": 318,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1116032694570311700,
+					"id_str": "1116032694570311680",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/D3zx0p2V4AAhJ8f.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/D3zx0p2V4AAhJ8f.jpg",
+					"url": "https://t.co/zsyBefQd2E",
+					"display_url": "pic.twitter.com/zsyBefQd2E",
+					"expanded_url": "https://twitter.com/wjsejguq_/status/1116032704720453632/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 424,
+							"h": 318,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 424,
+							"h": 318,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 424,
+							"h": 318,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							4,
+							3
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/D3zx0p2V4AAhJ8f.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2991653138,
+			"id_str": "2991653138",
+			"name": "전차도🌈",
+			"screen_name": "wjsejguq_",
+			"location": "사찰그만하구팔로우하쟈",
+			"description": "인용시비 즉각블락/세상은 아름답고 역사는 발전한다/#TransRightsAreHumanRights",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1871,
+			"friends_count": 674,
+			"listed_count": 36,
+			"created_at": "Wed Jan 21 15:43:46 +0000 2015",
+			"favourites_count": 15258,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 329760,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "89C9FA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1085334950969131008/mUVy3Jwe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1085334950969131008/mUVy3Jwe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2991653138/1553167222",
+			"profile_link_color": "89C9FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/zsyBefQd2E"
+	},
+	{
 		"created_at": "Wed Apr 10 05:50:53 +0000 2019",
 		"id": 1115116928941428700,
 		"id_str": "1115854643421224962",
