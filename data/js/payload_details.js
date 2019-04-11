380c83aabb1e06ec40f811f36b3ed474861ff985
Yeoncomi <yeoncomi@gmail.com>
2019-04-11 14:17:02 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 30867,
-	"created_at": "Thu Apr 11 18:17:01 +0000 2019",
+	"tweets": 30868,
+	"created_at": "Thu Apr 11 21:17:01 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Thu Apr 11 20:28:14 +0000 2019",
+		"id": 1116348885440446500,
+		"id_str": "1116437826738909184",
+		"full_text": "【성우에게 배게영업이란 게 정말로 있나요?】\nhttps://t.co/GRlY6cfcJq\n\n케모노 프렌즈2의 성우 오디션 대본이 자신이 쓴 대본의 무단 도용임을 밝히고 항의했다가 대본을 만든 AGN(야쿠자 회사)의 임원에게 '사과할 테니 만나자'는 말을 들은 후 6개월 이상 행방불명 상태인 아카리 선생님의 과거 글",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/GRlY6cfcJq",
+					"expanded_url": "https://gall.dcinside.com/mgallery/board/view/?id=kemono&no=287820",
+					"display_url": "gall.dcinside.com/mgallery/board…",
+					"indices": [
+						25,
+						48
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2915583102,
+			"id_str": "2915583102",
+			"name": "니현식",
+			"screen_name": "maidsuck",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 417,
+			"friends_count": 231,
+			"listed_count": 10,
+			"created_at": "Mon Dec 01 12:54:39 +0000 2014",
+			"favourites_count": 183,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 8791,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/798142641464451078/X3RSq8lz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/798142641464451078/X3RSq8lz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2915583102/1477392594",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【성우에게 배게영업이란 게 정말로 있나요?】\nhttps://t.co/GRlY6cfcJq\n\n케모노 프렌즈2의 성우 오디션 대본이 자신이 쓴 대본의 무단 도용임을 밝히고 항의했다가 대본을 만든 AGN(야쿠자 회사)의 임원에게 '사과할 테니 만나자'는 말을 들은 후 6개월 이상 행방불명 상태인 아카리 선생님의 과거 글"
+	},
+	{
 		"created_at": "Thu Apr 11 17:25:09 +0000 2019",
 		"id": 1116386361680986100,
 		"id_str": "1116391751999078400",
