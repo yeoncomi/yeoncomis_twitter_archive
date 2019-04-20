30a620b95be39e8e221c03603e6669370d5739c2
Yeoncomi <yeoncomi@gmail.com>
2019-04-19 22:17:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31264,
-	"created_at": "Sat Apr 20 04:17:04 +0000 2019",
+	"tweets": 31265,
+	"created_at": "Sat Apr 20 05:17:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 20 04:50:23 +0000 2019",
+		"id": 1119344680078938100,
+		"id_str": "1119463297802092544",
+		"full_text": "트위터의 흔한 애니프사 https://t.co/skhY8IAVHY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			12
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1119344643362000900,
+					"id_str": "1119344643362000896",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/D4i2BcjUEAABfXN.png",
+					"media_url_https": "https://pbs.twimg.com/media/D4i2BcjUEAABfXN.png",
+					"url": "https://t.co/skhY8IAVHY",
+					"display_url": "pic.twitter.com/skhY8IAVHY",
+					"expanded_url": "https://twitter.com/Erel_P/status/1119344680078938112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 602,
+							"h": 387,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 602,
+							"h": 387,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 602,
+							"h": 387,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1119344643362000900,
+					"id_str": "1119344643362000896",
+					"indices": [
+						13,
+						36
+					],
+					"media_url": "http://pbs.twimg.com/media/D4i2BcjUEAABfXN.png",
+					"media_url_https": "https://pbs.twimg.com/media/D4i2BcjUEAABfXN.png",
+					"url": "https://t.co/skhY8IAVHY",
+					"display_url": "pic.twitter.com/skhY8IAVHY",
+					"expanded_url": "https://twitter.com/Erel_P/status/1119344680078938112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 602,
+							"h": 387,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 602,
+							"h": 387,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 602,
+							"h": 387,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2898718081,
+			"id_str": "2898718081",
+			"name": "Erel_P ζ*'ヮ')ζ (川ヮO)∧",
+			"screen_name": "Erel_P",
+			"location": "",
+			"description": "에렐P 입니다. 야요이와 코우메 P 입니다. 잘 부탁드려요. エレルPですぇ.　やよいと小梅Pです. よろしくお願いします.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 747,
+			"friends_count": 1022,
+			"listed_count": 6,
+			"created_at": "Thu Nov 13 18:32:07 +0000 2014",
+			"favourites_count": 91237,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 105475,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F39939",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1110810121091768321/_PyKfm3__normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1110810121091768321/_PyKfm3__normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2898718081/1551378814",
+			"profile_link_color": "F39939",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 419,
+		"favorite_count": 84,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "트위터의 흔한 애니프사 https://t.co/skhY8IAVHY"
+	},
+	{
 		"created_at": "Sat Apr 20 03:37:07 +0000 2019",
 		"id": 1119444859977228300,
 		"id_str": "1119444859977228288",
