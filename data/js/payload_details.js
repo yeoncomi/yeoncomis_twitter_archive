1d87cf7bf760dd3631678abfdde4ecfabfda0a59
Yeoncomi <yeoncomi@gmail.com>
2019-04-19 17:17:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 31256,
-	"created_at": "Fri Apr 19 19:17:04 +0000 2019",
+	"tweets": 31257,
+	"created_at": "Sat Apr 20 00:17:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 20 00:16:06 +0000 2019",
+		"id": 1119142216813797400,
+		"id_str": "1119394274087292928",
+		"full_text": "?😊 https://t.co/kjxmeDT1xi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			2
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1119142210920837100,
+					"id_str": "1119142210920837121",
+					"indices": [
+						3,
+						26
+					],
+					"media_url": "http://pbs.twimg.com/media/D4f96U8U8AE7jDk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4f96U8U8AE7jDk.jpg",
+					"url": "https://t.co/kjxmeDT1xi",
+					"display_url": "pic.twitter.com/kjxmeDT1xi",
+					"expanded_url": "https://twitter.com/songjin9931/status/1119142216813797376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 496,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1241,
+							"h": 906,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 876,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1119142210920837100,
+					"id_str": "1119142210920837121",
+					"indices": [
+						3,
+						26
+					],
+					"media_url": "http://pbs.twimg.com/media/D4f96U8U8AE7jDk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4f96U8U8AE7jDk.jpg",
+					"url": "https://t.co/kjxmeDT1xi",
+					"display_url": "pic.twitter.com/kjxmeDT1xi",
+					"expanded_url": "https://twitter.com/songjin9931/status/1119142216813797376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 496,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1241,
+							"h": 906,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 876,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3002877439,
+			"id_str": "3002877439",
+			"name": "SONGJIN",
+			"screen_name": "songjin9931",
+			"location": "",
+			"description": "✨송진 ✨paper artist✨문의: songjin9931@gmail.com ✨페이퍼아트 키트 및 입체 특허 : 1020170146146",
+			"url": "https://t.co/W7xzLxmuAY",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/W7xzLxmuAY",
+							"expanded_url": "https://www.youtube.com/channel/UC4bblVzGc3wMaxdoIa6jsrQ",
+							"display_url": "youtube.com/channel/UC4bbl…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 32306,
+			"friends_count": 992,
+			"listed_count": 231,
+			"created_at": "Fri Jan 30 05:08:31 +0000 2015",
+			"favourites_count": 4532,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3442,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112733336827363328/P3Zd26ZK_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112733336827363328/P3Zd26ZK_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3002877439/1529662042",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1927,
+		"favorite_count": 257,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "?😊 https://t.co/kjxmeDT1xi"
+	},
+	{
 		"created_at": "Fri Apr 19 18:27:24 +0000 2019",
 		"id": 1119306520494108700,
 		"id_str": "1119306520494108672",
