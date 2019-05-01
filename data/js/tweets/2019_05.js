1de2f6142971ae787aa956394efca17c99a98aec
Yeoncomi <yeoncomi@gmail.com>
2019-04-30 18:17:08 -0700

    Added 1 tweets

---
+++ data/js/tweets/2019_05.js
@@ -0,0 +1,177 @@
+Grailbird.data.tweets_2019_05 = 
+[
+	{
+		"created_at": "Wed May 01 00:24:53 +0000 2019",
+		"id": 1123182479362248700,
+		"id_str": "1123382747978915840",
+		"full_text": "유자 직원임??? https://t.co/fnBeV5LGAB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123182466523586600,
+					"id_str": "1123182466523586560",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/media/D5ZYgJ8V4AAyTft.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5ZYgJ8V4AAyTft.jpg",
+					"url": "https://t.co/fnBeV5LGAB",
+					"display_url": "pic.twitter.com/fnBeV5LGAB",
+					"expanded_url": "https://twitter.com/Natsunokori/status/1123182479362248709/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 379,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 669,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1141,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123182466523586600,
+					"id_str": "1123182466523586560",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/media/D5ZYgJ8V4AAyTft.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5ZYgJ8V4AAyTft.jpg",
+					"url": "https://t.co/fnBeV5LGAB",
+					"display_url": "pic.twitter.com/fnBeV5LGAB",
+					"expanded_url": "https://twitter.com/Natsunokori/status/1123182479362248709/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 379,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 669,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1141,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1969146632,
+			"id_str": "1969146632",
+			"name": "하빙",
+			"screen_name": "Natsunokori",
+			"location": "夏氷",
+			"description": "게임, 야겜, 드라마 // 생각 정리 : @Summerlce",
+			"url": "https://t.co/TIpdfvgv32",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TIpdfvgv32",
+							"expanded_url": "http://summer-ice.tistory.com/1",
+							"display_url": "summer-ice.tistory.com/1",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 627,
+			"friends_count": 300,
+			"listed_count": 10,
+			"created_at": "Fri Oct 18 16:06:59 +0000 2013",
+			"favourites_count": 17,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 304216,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme19/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme19/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/938374195548848129/OhduZVF3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/938374195548848129/OhduZVF3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1969146632/1512560205",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": true,
+		"lang": "ko",
+		"text": "유자 직원임??? https://t.co/fnBeV5LGAB"
+	}
+]
