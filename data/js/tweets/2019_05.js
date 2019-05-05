3a0c478b57da1677d9d93e4921b625efbca5e6b3
Yeoncomi <yeoncomi@gmail.com>
2019-05-05 13:17:00 -0700

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 05 19:37:20 +0000 2019",
+		"id": 1123013823395328000,
+		"id_str": "1125122325781762048",
+		"full_text": "인간극장 99세 할아버지의 장수비결\n\n롯데야구 보면서 화 안내기 https://t.co/WCoZ6TrhAj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1123013641249288200,
+					"id_str": "1123013641249288192",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/media/D5W-9OyU8AAQbVO.png",
+					"media_url_https": "https://pbs.twimg.com/media/D5W-9OyU8AAQbVO.png",
+					"url": "https://t.co/WCoZ6TrhAj",
+					"display_url": "pic.twitter.com/WCoZ6TrhAj",
+					"expanded_url": "https://twitter.com/milanesanapolin/status/1123013823395328000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 690,
+							"h": 386,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 380,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 690,
+							"h": 386,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1123013641249288200,
+					"id_str": "1123013641249288192",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/media/D5W-9OyU8AAQbVO.png",
+					"media_url_https": "https://pbs.twimg.com/media/D5W-9OyU8AAQbVO.png",
+					"url": "https://t.co/WCoZ6TrhAj",
+					"display_url": "pic.twitter.com/WCoZ6TrhAj",
+					"expanded_url": "https://twitter.com/milanesanapolin/status/1123013823395328000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 690,
+							"h": 386,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 380,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 690,
+							"h": 386,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1123013693761921000,
+					"id_str": "1123013693761921024",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/media/D5W_ASaUIAAGIqn.png",
+					"media_url_https": "https://pbs.twimg.com/media/D5W_ASaUIAAGIqn.png",
+					"url": "https://t.co/WCoZ6TrhAj",
+					"display_url": "pic.twitter.com/WCoZ6TrhAj",
+					"expanded_url": "https://twitter.com/milanesanapolin/status/1123013823395328000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 688,
+							"h": 392,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 688,
+							"h": 392,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 387,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1123013753337815000,
+					"id_str": "1123013753337815040",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/media/D5W_DwWUIAA-Dgv.png",
+					"media_url_https": "https://pbs.twimg.com/media/D5W_DwWUIAA-Dgv.png",
+					"url": "https://t.co/WCoZ6TrhAj",
+					"display_url": "pic.twitter.com/WCoZ6TrhAj",
+					"expanded_url": "https://twitter.com/milanesanapolin/status/1123013823395328000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 684,
+							"h": 385,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 684,
+							"h": 385,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				},
+				{
+					"id": 1123013800343425000,
+					"id_str": "1123013800343425024",
+					"indices": [
+						36,
+						59
+					],
+					"media_url": "http://pbs.twimg.com/media/D5W_GfdU0AAfEpD.png",
+					"media_url_https": "https://pbs.twimg.com/media/D5W_GfdU0AAfEpD.png",
+					"url": "https://t.co/WCoZ6TrhAj",
+					"display_url": "pic.twitter.com/WCoZ6TrhAj",
+					"expanded_url": "https://twitter.com/milanesanapolin/status/1123013823395328000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 675,
+							"h": 388,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 675,
+							"h": 388,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 675,
+							"h": 388,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 4495381942,
+			"id_str": "4495381942",
+			"name": "문과형 테크프리스트 오리마니아",
+			"screen_name": "milanesanapolin",
+			"location": "",
+			"description": "비밀결사단 문사철",
+			"url": "https://t.co/5ivyjzFd1i",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5ivyjzFd1i",
+							"expanded_url": "http://soviet.com",
+							"display_url": "soviet.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1917,
+			"friends_count": 493,
+			"listed_count": 40,
+			"created_at": "Tue Dec 15 19:54:09 +0000 2015",
+			"favourites_count": 21531,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 163926,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1120593100387172352/KKSzi2vy_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1120593100387172352/KKSzi2vy_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/4495381942/1516852931",
+			"profile_link_color": "FAB81E",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13509,
+		"favorite_count": 4359,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "인간극장 99세 할아버지의 장수비결\n\n롯데야구 보면서 화 안내기 https://t.co/WCoZ6TrhAj"
+	},
+	{
 		"created_at": "Sun May 05 19:16:44 +0000 2019",
 		"id": 1125114074683981800,
 		"id_str": "1125117139092852736",
