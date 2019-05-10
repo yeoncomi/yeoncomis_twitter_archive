fb8be1a90c7121ee9e8984c053f265d257d528b1
Yeoncomi <yeoncomi@gmail.com>
2019-05-09 23:17:03 -0700

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 10 05:35:47 +0000 2019",
+		"id": 1126488837343965200,
+		"id_str": "1126722481157361666",
+		"full_text": "아무리 무례한 질문을 쏟아내도 자신에게 절대 불이익을 주지 않을 사람좋은 대통령 앞에서만 무례한 질문을 쏟아내는 KBS 송현정 기자가 있다. https://t.co/RGJcGyVuka",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			78
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1126488828628181000,
+					"id_str": "1126488828628180992",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/D6IXnwnUIAArUwz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6IXnwnUIAArUwz.jpg",
+					"url": "https://t.co/RGJcGyVuka",
+					"display_url": "pic.twitter.com/RGJcGyVuka",
+					"expanded_url": "https://twitter.com/2ppunippuni/status/1126488837343965184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 647,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1182,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 1182,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1126488828628181000,
+					"id_str": "1126488828628180992",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/D6IXnwnUIAArUwz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6IXnwnUIAArUwz.jpg",
+					"url": "https://t.co/RGJcGyVuka",
+					"display_url": "pic.twitter.com/RGJcGyVuka",
+					"expanded_url": "https://twitter.com/2ppunippuni/status/1126488837343965184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 647,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1182,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 1182,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1126488828603011100,
+					"id_str": "1126488828603011073",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/D6IXnwhUEAEiM4H.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D6IXnwhUEAEiM4H.jpg",
+					"url": "https://t.co/RGJcGyVuka",
+					"display_url": "pic.twitter.com/RGJcGyVuka",
+					"expanded_url": "https://twitter.com/2ppunippuni/status/1126488837343965184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 629,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1108,
+							"h": 1198,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1108,
+							"h": 1198,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2938444820,
+			"id_str": "2938444820",
+			"name": "신비아파트",
+			"screen_name": "2ppunippuni",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 135,
+			"friends_count": 179,
+			"listed_count": 0,
+			"created_at": "Sun Dec 21 16:26:43 +0000 2014",
+			"favourites_count": 558,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 291,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/991325059640799234/kBYJG-rW_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/991325059640799234/kBYJG-rW_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2938444820/1521274331",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1692,
+		"favorite_count": 883,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "아무리 무례한 질문을 쏟아내도 자신에게 절대 불이익을 주지 않을 사람좋은 대통령 앞에서만 무례한 질문을 쏟아내는 KBS 송현정 기자가 있다. https://t.co/RGJcGyVuka"
+	},
+	{
 		"created_at": "Fri May 10 02:25:34 +0000 2019",
 		"id": 1126674610571792400,
 		"id_str": "1126674610571792385",
