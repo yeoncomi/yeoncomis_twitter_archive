bce825c21e38ff132635e309a7c41254a3da795b
Yeoncomi <yeoncomi@gmail.com>
2019-05-20 21:17:08 -0700

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32428,
-	"created_at": "Tue May 21 03:17:03 +0000 2019",
+	"tweets": 32431,
+	"created_at": "Tue May 21 04:17:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,528 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Tue May 21 03:44:32 +0000 2019",
+		"id": 1130680452069695500,
+		"id_str": "1130680750842343424",
+		"full_text": "i love the girls anthology event https://t.co/oQP24QnaNe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			32
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1130680448219320300,
+					"id_str": "1130680448219320321",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/D7D74IaXkAEJAlC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7D74IaXkAEJAlC.jpg",
+					"url": "https://t.co/oQP24QnaNe",
+					"display_url": "pic.twitter.com/oQP24QnaNe",
+					"expanded_url": "https://twitter.com/badbandoriposts/status/1130680452069695493/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1130680448219320300,
+					"id_str": "1130680448219320321",
+					"indices": [
+						33,
+						56
+					],
+					"media_url": "http://pbs.twimg.com/media/D7D74IaXkAEJAlC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7D74IaXkAEJAlC.jpg",
+					"url": "https://t.co/oQP24QnaNe",
+					"display_url": "pic.twitter.com/oQP24QnaNe",
+					"expanded_url": "https://twitter.com/badbandoriposts/status/1130680452069695493/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 400,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 400,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1017584261086367700,
+			"id_str": "1017584261086367745",
+			"name": "bad bandori posts",
+			"screen_name": "badbandoriposts",
+			"location": "",
+			"description": "b̃͋̾r͎̗̥eͬͬ̃a͊̾ͤd̍̒̈ ? bread.",
+			"url": "https://t.co/cVMG7n07F8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cVMG7n07F8",
+							"expanded_url": "https://badbandoriposts.carrd.co/#",
+							"display_url": "badbandoriposts.carrd.co/#",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8936,
+			"friends_count": 3,
+			"listed_count": 22,
+			"created_at": "Fri Jul 13 01:39:28 +0000 2018",
+			"favourites_count": 1,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 366,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112870448801300481/uxSJkRZJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112870448801300481/uxSJkRZJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1017584261086367745/1554163967",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 79,
+		"favorite_count": 180,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "i love the girls anthology event https://t.co/oQP24QnaNe"
+	},
+	{
+		"created_at": "Tue May 21 03:42:46 +0000 2019",
+		"id": 1130636100224016400,
+		"id_str": "1130680307361779717",
+		"full_text": "사람들이 탈원전에 의문을 가지려 하는 타이밍에 늘 원전 관계자들은 탈원전의 죽창을 꽂아댄다.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1110535327825973200,
+			"id_str": "1110535327825973248",
+			"name": "中山",
+			"screen_name": "DrSunYat_Sen",
+			"location": "",
+			"description": "다시 돌아왔다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 315,
+			"friends_count": 92,
+			"listed_count": 7,
+			"created_at": "Tue Mar 26 13:33:49 +0000 2019",
+			"favourites_count": 3,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 878,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1110546373147947008/5Nx3OG-r_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1110546373147947008/5Nx3OG-r_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1110535327825973248/1553610866",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 74,
+		"favorite_count": 19,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "ko",
+		"text": "사람들이 탈원전에 의문을 가지려 하는 타이밍에 늘 원전 관계자들은 탈원전의 죽창을 꽂아댄다."
+	},
+	{
+		"created_at": "Tue May 21 03:41:41 +0000 2019",
+		"id": 1130582611447885800,
+		"id_str": "1130680033587032064",
+		"full_text": "놀란의 영화가 성공하는 이유 https://t.co/Ujm3KKoSB2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			15
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1130582606548914200,
+					"id_str": "1130582606548914176",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D7Ci4_yVUAAqIRP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7Ci4_yVUAAqIRP.jpg",
+					"url": "https://t.co/Ujm3KKoSB2",
+					"display_url": "pic.twitter.com/Ujm3KKoSB2",
+					"expanded_url": "https://twitter.com/TimeinPnk/status/1130582611447885824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1130582606548914200,
+					"id_str": "1130582606548914176",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D7Ci4_yVUAAqIRP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7Ci4_yVUAAqIRP.jpg",
+					"url": "https://t.co/Ujm3KKoSB2",
+					"display_url": "pic.twitter.com/Ujm3KKoSB2",
+					"expanded_url": "https://twitter.com/TimeinPnk/status/1130582611447885824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1130582606485917700,
+					"id_str": "1130582606485917696",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D7Ci4_jUEAA7yaP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7Ci4_jUEAA7yaP.jpg",
+					"url": "https://t.co/Ujm3KKoSB2",
+					"display_url": "pic.twitter.com/Ujm3KKoSB2",
+					"expanded_url": "https://twitter.com/TimeinPnk/status/1130582611447885824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1130582606481768400,
+					"id_str": "1130582606481768449",
+					"indices": [
+						16,
+						39
+					],
+					"media_url": "http://pbs.twimg.com/media/D7Ci4_iUwAEQYDz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7Ci4_iUwAEQYDz.jpg",
+					"url": "https://t.co/Ujm3KKoSB2",
+					"display_url": "pic.twitter.com/Ujm3KKoSB2",
+					"expanded_url": "https://twitter.com/TimeinPnk/status/1130582611447885824/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 877445354232324100,
+			"id_str": "877445354232324096",
+			"name": "금천구 소드 오프 Pnk",
+			"screen_name": "TimeinPnk",
+			"location": "대한민국 금천구",
+			"description": "눈앞에 이익을 보거든 먼저 그것을 취함이 의리에 합당한 지를 생각하라 /언팔 무조건 추적블락/반문친유/블락 문의 뒷계로 디엠 @warmsweetwater",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 281,
+			"friends_count": 1651,
+			"listed_count": 0,
+			"created_at": "Wed Jun 21 08:37:29 +0000 2017",
+			"favourites_count": 6501,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 17679,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1128710701353132034/ZHZzCXhv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1128710701353132034/ZHZzCXhv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/877445354232324096/1557940376",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0112159849aed9ab",
+			"url": "https://api.twitter.com/1.1/geo/id/0112159849aed9ab.json",
+			"place_type": "city",
+			"name": "Gimpo-si",
+			"full_name": "Gimpo-si, Republic of Korea",
+			"country_code": "KR",
+			"country": "Republic of Korea",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							126.517161547291,
+							37.5324876480762
+						],
+						[
+							126.808799250058,
+							37.5324876480762
+						],
+						[
+							126.808799250058,
+							37.7765099020653
+						],
+						[
+							126.517161547291,
+							37.7765099020653
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 127,
+		"favorite_count": 44,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "놀란의 영화가 성공하는 이유 https://t.co/Ujm3KKoSB2"
+	},
+	{
 		"created_at": "Tue May 21 03:16:37 +0000 2019",
 		"id": 1130638065825853400,
 		"id_str": "1130673723629948928",
