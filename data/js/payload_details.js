c421ad8bb4a80f590c4673170a950db6a2e0ce9f
Yeoncomi <yeoncomi@gmail.com>
2019-05-22 09:17:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32453,
-	"created_at": "Wed May 22 15:17:05 +0000 2019",
+	"tweets": 32454,
+	"created_at": "Wed May 22 16:17:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,203 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 22 15:19:11 +0000 2019",
+		"id": 1131068175615909900,
+		"id_str": "1131217952445911040",
+		"full_text": "건국머 팀플 레전드 https://t.co/4NtUHFgrKa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			10
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1131068153901985800,
+					"id_str": "1131068153901985792",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/media/D7JcfkoVsAAdFCT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7JcfkoVsAAdFCT.jpg",
+					"url": "https://t.co/4NtUHFgrKa",
+					"display_url": "pic.twitter.com/4NtUHFgrKa",
+					"expanded_url": "https://twitter.com/Rotriever/status/1131068175615909888/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 1480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 584,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 331,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1131068153901985800,
+					"id_str": "1131068153901985792",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/media/D7JcfkoVsAAdFCT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7JcfkoVsAAdFCT.jpg",
+					"url": "https://t.co/4NtUHFgrKa",
+					"display_url": "pic.twitter.com/4NtUHFgrKa",
+					"expanded_url": "https://twitter.com/Rotriever/status/1131068175615909888/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 720,
+							"h": 1480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 584,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 331,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1131068167730606100,
+					"id_str": "1131068167730606080",
+					"indices": [
+						11,
+						34
+					],
+					"media_url": "http://pbs.twimg.com/media/D7JcgYJVsAAM_HZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7JcgYJVsAAM_HZ.jpg",
+					"url": "https://t.co/4NtUHFgrKa",
+					"display_url": "pic.twitter.com/4NtUHFgrKa",
+					"expanded_url": "https://twitter.com/Rotriever/status/1131068175615909888/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 584,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 331,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 720,
+							"h": 1480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1114701997922345000,
+			"id_str": "1114701997922344960",
+			"name": "로트리버",
+			"screen_name": "Rotriever",
+			"location": "대한민국",
+			"description": "앞서서 나가니 산자여 따르라",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 172,
+			"friends_count": 315,
+			"listed_count": 1,
+			"created_at": "Sun Apr 07 01:30:41 +0000 2019",
+			"favourites_count": 1105,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1769,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1129344776745078784/wGqqHZML_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1129344776745078784/wGqqHZML_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1114701997922344960/1554618810",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 354,
+		"favorite_count": 73,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "건국머 팀플 레전드 https://t.co/4NtUHFgrKa"
+	},
+	{
 		"created_at": "Wed May 22 15:16:58 +0000 2019",
 		"id": 1131018766488203300,
 		"id_str": "1131217395484250112",
