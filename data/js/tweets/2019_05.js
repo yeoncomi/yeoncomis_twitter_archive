cc836b40af23732f545323262a1ee8093140053f
Yeoncomi <yeoncomi@gmail.com>
2019-05-27 16:17:06 -0700

    Added 2 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,363 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 27 23:11:58 +0000 2019",
+		"id": 1133021901981282300,
+		"id_str": "1133148871415148544",
+		"full_text": "1941년 6월 30일,취사병인 세레다는 점심을 준비하다가 갑자기 38(t)전차가 들이닥쳐 점심을 엎어버리자 도끼를 들고\n전차를 계속 내리쳤고 전차병들은 결국 항복했다...\n?????????????? https://t.co/HYzdOMZ07h",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1133021887427108900,
+					"id_str": "1133021887427108868",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/D7lNZ1LV4AQ_MPZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7lNZ1LV4AQ_MPZ.jpg",
+					"url": "https://t.co/HYzdOMZ07h",
+					"display_url": "pic.twitter.com/HYzdOMZ07h",
+					"expanded_url": "https://twitter.com/damcho97/status/1133021901981282305/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 420,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 420,
+							"h": 454,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 420,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1133021887427108900,
+					"id_str": "1133021887427108868",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/D7lNZ1LV4AQ_MPZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7lNZ1LV4AQ_MPZ.jpg",
+					"url": "https://t.co/HYzdOMZ07h",
+					"display_url": "pic.twitter.com/HYzdOMZ07h",
+					"expanded_url": "https://twitter.com/damcho97/status/1133021901981282305/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 420,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 420,
+							"h": 454,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 420,
+							"h": 454,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1133021899087175700,
+					"id_str": "1133021899087175680",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/D7lNagnUYAAJ8dS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7lNagnUYAAJ8dS.jpg",
+					"url": "https://t.co/HYzdOMZ07h",
+					"display_url": "pic.twitter.com/HYzdOMZ07h",
+					"expanded_url": "https://twitter.com/damcho97/status/1133021901981282305/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 420,
+							"h": 221,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 420,
+							"h": 221,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 420,
+							"h": 221,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1044759049475043300,
+			"id_str": "1044759049475043328",
+			"name": "하와와의 증인 맛있담쵸",
+			"screen_name": "damcho97",
+			"location": "",
+			"description": "참! 잘했어요",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 127,
+			"friends_count": 153,
+			"listed_count": 9,
+			"created_at": "Wed Sep 26 01:22:22 +0000 2018",
+			"favourites_count": 8466,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 5511,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1132224585913458688/jHiZ99-v_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1132224585913458688/jHiZ99-v_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1044759049475043328/1556290610",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 94,
+		"favorite_count": 38,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "1941년 6월 30일,취사병인 세레다는 점심을 준비하다가 갑자기 38(t)전차가 들이닥쳐 점심을 엎어버리자 도끼를 들고\n전차를 계속 내리쳤고 전차병들은 결국 항복했다...\n?????????????? https://t.co/HYzdOMZ07h"
+	},
+	{
+		"created_at": "Mon May 27 23:11:26 +0000 2019",
+		"id": 1133144099790282800,
+		"id_str": "1133148737138593792",
+		"full_text": "https://t.co/STXiMMzC4K",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			0
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1133144090638397400,
+					"id_str": "1133144090638397440",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D7m8i_nVsAAvNvA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7m8i_nVsAAvNvA.jpg",
+					"url": "https://t.co/STXiMMzC4K",
+					"display_url": "pic.twitter.com/STXiMMzC4K",
+					"expanded_url": "https://twitter.com/ygKSw469uX38uU8/status/1133144099790282752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 882,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 586,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 882,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1133144090638397400,
+					"id_str": "1133144090638397440",
+					"indices": [
+						0,
+						23
+					],
+					"media_url": "http://pbs.twimg.com/media/D7m8i_nVsAAvNvA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7m8i_nVsAAvNvA.jpg",
+					"url": "https://t.co/STXiMMzC4K",
+					"display_url": "pic.twitter.com/STXiMMzC4K",
+					"expanded_url": "https://twitter.com/ygKSw469uX38uU8/status/1133144099790282752/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1024,
+							"h": 882,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 586,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 882,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1103652207277203500,
+			"id_str": "1103652207277203457",
+			"name": "깔깔갤러리",
+			"screen_name": "ygKSw469uX38uU8",
+			"location": "중복이 있을수 있음/마음함 보세요",
+			"description": "짤제보 하고싶으면 주세요 단 내가 노잼이면 안올림",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 64647,
+			"friends_count": 0,
+			"listed_count": 103,
+			"created_at": "Thu Mar 07 13:42:45 +0000 2019",
+			"favourites_count": 5,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112717877566558208/59L-vEko_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112717877566558208/59L-vEko_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 470,
+		"favorite_count": 134,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/STXiMMzC4K"
+	},
+	{
 		"created_at": "Mon May 27 20:07:16 +0000 2019",
 		"id": 1133044689559588900,
 		"id_str": "1133102388825346048",
