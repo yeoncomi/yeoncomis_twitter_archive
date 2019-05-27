2cde3de53ca3bbfbebc9870ada4133099b6288a4
Yeoncomi <yeoncomi@gmail.com>
2019-05-27 08:17:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32664,
-	"created_at": "Mon May 27 05:17:05 +0000 2019",
+	"tweets": 32665,
+	"created_at": "Mon May 27 15:17:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 27 14:51:11 +0000 2019",
+		"id": 1132854176218665000,
+		"id_str": "1133022846655983618",
+		"full_text": "암드 컴퓨텍스 키노트 요약 https://t.co/B2WgnVpYU6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			14
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1132854090197717000,
+					"id_str": "1132854090197716992",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/D7i0yvyVUAAUKcE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7i0yvyVUAAUKcE.jpg",
+					"url": "https://t.co/B2WgnVpYU6",
+					"display_url": "pic.twitter.com/B2WgnVpYU6",
+					"expanded_url": "https://twitter.com/xovoxdog/status/1132854176218664961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 932,
+							"h": 517,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 932,
+							"h": 517,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 377,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1132854090197717000,
+					"id_str": "1132854090197716992",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/D7i0yvyVUAAUKcE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7i0yvyVUAAUKcE.jpg",
+					"url": "https://t.co/B2WgnVpYU6",
+					"display_url": "pic.twitter.com/B2WgnVpYU6",
+					"expanded_url": "https://twitter.com/xovoxdog/status/1132854176218664961/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 932,
+							"h": 517,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 932,
+							"h": 517,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 377,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 153662415,
+			"id_str": "153662415",
+			"name": "DOGGyRADIO",
+			"screen_name": "xovoxdog",
+			"location": "Daegu",
+			"description": "닉값하는 트윗. 취미로 짤도 그림.\nKookmin Univ. Law 09'",
+			"url": "https://t.co/zG4uYS8gbb",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zG4uYS8gbb",
+							"expanded_url": "http://pixiv.me/xovoxdog",
+							"display_url": "pixiv.me/xovoxdog",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 223,
+			"friends_count": 129,
+			"listed_count": 7,
+			"created_at": "Wed Jun 09 03:53:28 +0000 2010",
+			"favourites_count": 60,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 89660,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1112733821135282177/z6Q_qMfc_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1112733821135282177/z6Q_qMfc_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/153662415/1503068669",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 55,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "암드 컴퓨텍스 키노트 요약 https://t.co/B2WgnVpYU6"
+	},
+	{
 		"created_at": "Mon May 27 04:37:53 +0000 2019",
 		"id": 1132865296836902900,
 		"id_str": "1132868501704601600",
