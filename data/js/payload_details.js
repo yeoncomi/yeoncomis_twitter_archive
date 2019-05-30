6fdbedf7287b37e4c3706795bfacd1986e31c89b
Yeoncomi <yeoncomi@gmail.com>
2019-05-29 21:17:08 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 32752,
-	"created_at": "Thu May 30 03:17:07 +0000 2019",
+	"tweets": 32753,
+	"created_at": "Thu May 30 04:17:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 30 04:10:41 +0000 2019",
+		"id": 1133681337846493200,
+		"id_str": "1133948820780228608",
+		"full_text": "요즘들어 중국어선 기사가 보이지 않는 이유\n\n빡세게 해서 다 잡아버림 https://t.co/4W5VzOsthW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1133681291897913300,
+					"id_str": "1133681291897913344",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/media/D7ulIOQUwAABhcZ.png",
+					"media_url_https": "https://pbs.twimg.com/media/D7ulIOQUwAABhcZ.png",
+					"url": "https://t.co/4W5VzOsthW",
+					"display_url": "pic.twitter.com/4W5VzOsthW",
+					"expanded_url": "https://twitter.com/go1rani/status/1133681337846493184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 255,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 300,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1133681291897913300,
+					"id_str": "1133681291897913344",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/media/D7ulIOQUwAABhcZ.png",
+					"media_url_https": "https://pbs.twimg.com/media/D7ulIOQUwAABhcZ.png",
+					"url": "https://t.co/4W5VzOsthW",
+					"display_url": "pic.twitter.com/4W5VzOsthW",
+					"expanded_url": "https://twitter.com/go1rani/status/1133681337846493184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 255,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 300,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 300,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1023826712822333400,
+			"id_str": "1023826712822333441",
+			"name": "무슨 일이 일어나고 있나요?",
+			"screen_name": "go1rani",
+			"location": "",
+			"description": "주로 답글창이 폭발하는 사회적 이슈를 다룹니다",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 35870,
+			"friends_count": 5,
+			"listed_count": 80,
+			"created_at": "Mon Jul 30 07:04:44 +0000 2018",
+			"favourites_count": 37,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1173,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1072328137793527808/S5Kh_giw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1072328137793527808/S5Kh_giw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1023826712822333441/1534055590",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 854,
+		"favorite_count": 381,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "요즘들어 중국어선 기사가 보이지 않는 이유\n\n빡세게 해서 다 잡아버림 https://t.co/4W5VzOsthW"
+	},
+	{
 		"created_at": "Thu May 30 02:29:41 +0000 2019",
 		"id": 1133923404938063900,
 		"id_str": "1133923404938063872",
