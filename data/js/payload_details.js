624391b43d9ae23db3a895939b36f384467f54a9
Yeoncomi <yeoncomi@gmail.com>
2019-06-06 14:17:01 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 33185,
-	"created_at": "Thu Jun 06 19:17:02 +0000 2019",
+	"tweets": 33186,
+	"created_at": "Thu Jun 06 21:17:00 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Thu Jun 06 20:57:47 +0000 2019",
+		"id": 1136687563421421600,
+		"id_str": "1136738981641502720",
+		"full_text": "THE CIRCULATION OF CAPITAL AS A WHOLE https://t.co/bj84O11kfd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			37
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1136687118112165900,
+					"id_str": "1136687118112165888",
+					"indices": [
+						38,
+						61
+					],
+					"media_url": "http://pbs.twimg.com/media/D8ZS6VfWkAAhoOV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8ZS6VfWkAAhoOV.jpg",
+					"url": "https://t.co/bj84O11kfd",
+					"display_url": "pic.twitter.com/bj84O11kfd",
+					"expanded_url": "https://twitter.com/HumansOfLate/status/1136687563421421570/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1136687118112165900,
+					"id_str": "1136687118112165888",
+					"indices": [
+						38,
+						61
+					],
+					"media_url": "http://pbs.twimg.com/media/D8ZS6VfWkAAhoOV.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8ZS6VfWkAAhoOV.jpg",
+					"url": "https://t.co/bj84O11kfd",
+					"display_url": "pic.twitter.com/bj84O11kfd",
+					"expanded_url": "https://twitter.com/HumansOfLate/status/1136687563421421570/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 818488898418602000,
+			"id_str": "818488898418601984",
+			"name": "David Harvey",
+			"screen_name": "HumansOfLate",
+			"location": "latecapitalism.official@gmail.com",
+			"description": "#humansoflatecapitalism",
+			"url": "https://t.co/sQ4cDsrL0s",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sQ4cDsrL0s",
+							"expanded_url": "https://www.instagram.com/humansoflate",
+							"display_url": "instagram.com/humansoflate",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 241174,
+			"friends_count": 1,
+			"listed_count": 1212,
+			"created_at": "Mon Jan 09 16:05:35 +0000 2017",
+			"favourites_count": 140,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 17217,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1077515812183908352/of72ql1t_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1077515812183908352/of72ql1t_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/818488898418601984/1517003084",
+			"profile_link_color": "6000FF",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 387,
+		"favorite_count": 1535,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "THE CIRCULATION OF CAPITAL AS A WHOLE https://t.co/bj84O11kfd"
+	},
+	{
 		"created_at": "Thu Jun 06 18:20:26 +0000 2019",
 		"id": 1136503330073526300,
 		"id_str": "1136699385213931520",
